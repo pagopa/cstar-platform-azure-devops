f9d9848636fa2e0321db017153a712c940e438e5
diegoitaliait <diego.lagosmorales@pagopa.it>
2022-06-07 13:20:01 +0200

    minor fix

--- azure-devops/idpay-projects/99_main.tf
+++ azure-devops/idpay-projects/99_main.tf
@@ -17,32 +17,32 @@ provider "azurerm" {
 }
 
 provider "azurerm" {
+  alias           = "dev"
   features {
     key_vault {
       purge_soft_delete_on_destroy = false
     }
   }
-  alias           = "dev"
   subscription_id = module.secret_azdo.values["PAGOPAIT-DEV-CSTAR-SUBSCRIPTION-ID"].value
 }
 
 provider "azurerm" {
+  alias           = "uat"
   features {
     key_vault {
       purge_soft_delete_on_destroy = false
     }
   }
-  alias           = "uat"
   subscription_id = module.secret_azdo.values["PAGOPAIT-UAT-CSTAR-SUBSCRIPTION-ID"].value
 }
 
 provider "azurerm" {
+  alias           = "prod"
   features {
     key_vault {
       purge_soft_delete_on_destroy = false
     }
   }
-  alias           = "prod"
   subscription_id = module.secret_azdo.values["PAGOPAIT-PROD-CSTAR-SUBSCRIPTION-ID"].value
 }
 
