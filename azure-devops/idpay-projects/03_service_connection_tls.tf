298cb999570565207a7013dc3738b24516e9b062
diegoitaliait <diego.lagosmorales@pagopa.it>
2022-06-07 13:19:49 +0200

    fix tls service connection

--- azure-devops/idpay-projects/03_service_connection_tls.tf
+++ azure-devops/idpay-projects/03_service_connection_tls.tf
@@ -1,12 +1,17 @@
 module "DEV-CSTAR-IDPAY-TLS-CERT-SERVICE-CONN" {
-  depends_on = [azuredevops_project.project]
+
+  providers = {
+    azurerm = azurerm.dev
+  }
+
+  depends_on = [data.azuredevops_project.project]
   source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_azurerm_limited?ref=v2.1.0"
 
-  project_id        = azuredevops_project.project.id
+  project_id        = data.azuredevops_project.project.id
   name              = "${local.prefix}-d-${local.domain}-tls-cert"
-  tenant_id         = module.secrets_azdo.values["PAGOPAIT-TENANTID"].value
+  tenant_id         = module.secret_azdo.values["PAGOPAIT-TENANTID"].value
   subscription_name = local.dev_cstar_subscription_name
-  subscription_id   = module.secrets_azdo.values["PAGOPAIT-DEV-CSTAR-SUBSCRIPTION-ID"].value
+  subscription_id   = module.secret_azdo.values["PAGOPAIT-DEV-CSTAR-SUBSCRIPTION-ID"].value
   #tfsec:ignore:GEN003
   renew_token = local.tlscert_renew_token
 
