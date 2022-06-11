e6d262b6079bc4476426635a14275331e5f8d7a6
diegoitaliait <diego.lagosmorales@pagopa.it>
2022-06-11 16:36:08 +0200

    fix pipeline name

--- azure-devops/idpay-projects/06_pipeline_devops-java-springboot-color.tf
+++ azure-devops/idpay-projects/06_pipeline_devops-java-springboot-color.tf
@@ -1,8 +1,8 @@
-variable "devops-springboot-gitlab-workflow" {
+variable "devops-java-springboot-color" {
   default = {
     repository = {
       organization    = "pagopa"
-      name            = "devops-springboot-gitlab-workflow"
+      name            = "devops-java-springboot-color"
       branch_name     = "release-dev"
       pipelines_path  = ".devops"
       yml_prefix_name = null
@@ -16,31 +16,31 @@ variable "devops-springboot-gitlab-workflow" {
 
 locals {
   # global vars
-  devops-springboot-gitlab-workflow-variables = {
+  devops-java-springboot-color-variables = {
     dockerfile = "Dockerfile"
   }
   # global secrets
-  devops-springboot-gitlab-workflow-variables_secret = {
+  devops-java-springboot-color-variables_secret = {
 
   }
   # code_review vars
-  devops-springboot-gitlab-workflow-variables_code_review = {
+  devops-java-springboot-color-variables_code_review = {
     sonarcloud_service_conn = "SONARCLOUD-SERVICE-CONN"
-    sonarcloud_org          = var.devops-springboot-gitlab-workflow.repository.organization
-    sonarcloud_project_key  = "${var.devops-springboot-gitlab-workflow.repository.organization}_${var.devops-springboot-gitlab-workflow.repository.name}"
-    sonarcloud_project_name = var.devops-springboot-gitlab-workflow.repository.name
+    sonarcloud_org          = var.devops-java-springboot-color.repository.organization
+    sonarcloud_project_key  = "${var.devops-java-springboot-color.repository.organization}_${var.devops-java-springboot-color.repository.name}"
+    sonarcloud_project_name = var.devops-java-springboot-color.repository.name
   }
   # code_review secrets
-  devops-springboot-gitlab-workflow-variables_secret_code_review = {
+  devops-java-springboot-color-variables_secret_code_review = {
 
   }
   # deploy vars
-  devops-springboot-gitlab-workflow-variables_deploy = {
-    K8S_IMAGE_REPOSITORY_NAME        = replace(var.devops-springboot-gitlab-workflow.repository.name, "-", "")
+  devops-java-springboot-color-variables_deploy = {
+    K8S_IMAGE_REPOSITORY_NAME        = replace(var.devops-java-springboot-color.repository.name, "-", "")
     DEPLOY_NAMESPACE                 = local.domain
     SETTINGS_XML_RW_SECURE_FILE_NAME = "settings-rw.xml"
     SETTINGS_XML_RO_SECURE_FILE_NAME = "settings-ro.xml"
-    HELM_RELEASE_NAME                = var.devops-springboot-gitlab-workflow.repository.name
+    HELM_RELEASE_NAME                = var.devops-java-springboot-color.repository.name
 
     DEV_CONTAINER_REGISTRY_SERVICE_CONN = local.service_endpoint_azure_devops_acr_aks_dev_name
     DEV_KUBERNETES_SERVICE_CONN         = local.srv_endpoint_name_aks_dev
@@ -56,29 +56,29 @@ locals {
     # PROD_AGENT_POOL                      = "cstar-prod-linux"
   }
   # deploy secrets
-  devops-springboot-gitlab-workflow-variables_secret_deploy = {
+  devops-java-springboot-color-variables_secret_deploy = {
 
   }
 }
 
-# module "devops-springboot-gitlab-workflow_code_review" {
+# module "devops-java-springboot-color_code_review" {
 #   source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_code_review?ref=v2.1.0"
-#   count  = var.devops-springboot-gitlab-workflow.pipeline.enable_code_review == true ? 1 : 0
+#   count  = var.devops-java-springboot-color.pipeline.enable_code_review == true ? 1 : 0
 
 #   project_id                   = azuredevops_project.project.id
-#   repository                   = var.devops-springboot-gitlab-workflow.repository
+#   repository                   = var.devops-java-springboot-color.repository
 #   github_service_connection_id = azuredevops_serviceendpoint_github.io-azure-devops-github-pr.id
 
 #   pull_request_trigger_use_yaml = true
 
 #   variables = merge(
-#     local.devops-springboot-gitlab-workflow-variables,
-#     local.devops-springboot-gitlab-workflow-variables_code_review,
+#     local.devops-java-springboot-color-variables,
+#     local.devops-java-springboot-color-variables_code_review,
 #   )
 
 #   variables_secret = merge(
-#     local.devops-springboot-gitlab-workflow-variables_secret,
-#     local.devops-springboot-gitlab-workflow-variables_secret_code_review,
+#     local.devops-java-springboot-color-variables_secret,
+#     local.devops-java-springboot-color-variables_secret_code_review,
 #   )
 
 #   service_connection_ids_authorization = [
@@ -87,24 +87,24 @@ locals {
 #   ]
 # }
 
-module "devops-springboot-gitlab-workflow_deploy" {
-  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_deploy?ref=v2.1.0"
-  count  = var.devops-springboot-gitlab-workflow.pipeline.enable_deploy == true ? 1 : 0
+module "devops-java-springboot-color_deploy" {
+  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_deploy?ref=fix-schedules-triggers"
+  count  = var.devops-java-springboot-color.pipeline.enable_deploy == true ? 1 : 0
 
   project_id                   = data.azuredevops_project.project.id
-  repository                   = var.devops-springboot-gitlab-workflow.repository
+  repository                   = var.devops-java-springboot-color.repository
   github_service_connection_id = local.service_endpoint_io_azure_devops_github_pr_id
 
   ci_trigger_use_yaml = true
 
   variables = merge(
-    local.devops-springboot-gitlab-workflow-variables,
-    local.devops-springboot-gitlab-workflow-variables_deploy,
+    local.devops-java-springboot-color-variables,
+    local.devops-java-springboot-color-variables_deploy,
   )
 
   variables_secret = merge(
-    local.devops-springboot-gitlab-workflow-variables_secret,
-    local.devops-springboot-gitlab-workflow-variables_secret_deploy,
+    local.devops-java-springboot-color-variables_secret,
+    local.devops-java-springboot-color-variables_secret_deploy,
   )
 
   service_connection_ids_authorization = [
