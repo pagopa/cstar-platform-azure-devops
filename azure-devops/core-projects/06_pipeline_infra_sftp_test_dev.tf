6e744284aed6b924593e17fa354ae8c6b8c8bfa4
Matteo Cellucci <matteo.cellucci@keypartner.com>
2022-06-27 10:04:55 +0200

    refactor: Create infra-sftp-test-dev pipeline

--- azure-devops/core-projects/06_pipeline_infra_sftp_test_dev.tf
+++ azure-devops/core-projects/06_pipeline_infra_sftp_test_dev.tf
@@ -1,42 +1,48 @@
-variable "sftp-test" {
+variable "infra-sftp-test-dev" {
   default = {
     repository = {
       organization    = "pagopa"
-      name            = "sftp-test"
-      branch_name     = "release-dev"
+      name            = "infra-sftp-test"
+      branch_name     = "main"
       pipelines_path  = ".devops"
       yml_prefix_name = null
     }
     pipeline = {
       enable_code_review = false
       enable_deploy      = true
-      path               = "cstar\\sftp-test"
+      path               = "cstar-dev"
     }
   }
 }
 
 locals {
   # global vars
-  sftp-test-variables = {
+  infra-sftp-test-dev-variables = {
     dockerfile = "Dockerfile"
   }
   # global secrets
-  sftp-test-variables_secret = {
+  infra-sftp-test-dev-variables_secret = {
 
   }
   # code_review vars
-  sftp-test-variables_code_review = {
+  infra-sftp-test-dev-variables_code_review = {
 
   }
   # code_review secrets
-  sftp-test-variables_secret_code_review = {
+  infra-sftp-test-dev-variables_secret_code_review = {
 
   }
   # deploy vars
-  sftp-test-variables_deploy = {
-    K8S_IMAGE_REPOSITORY_NAME        = replace(var.sftp-test.repository.name, "-", "")
+  infra-sftp-test-dev-variables_deploy = {
+    environment                     = 'DEV'
+    dockerRegistryServiceConnection = local.service_endpoint_azure_devops_acr_aks_dev_name
+    kubernetesServiceConnection     = local.srv_endpoint_name_aks_dev
+    containerRegistry               = local.aks_cr_name_dev
+    selfHostedAgentPool             = local.azdo_agent_pool_dev
+
+    K8S_IMAGE_REPOSITORY_NAME        = replace(var.infra-sftp-test-dev.repository.name, "-", "")
     DEPLOY_NAMESPACE                 = local.domain
-    HELM_RELEASE_NAME                = var.sftp-test.repository.name
+    HELM_RELEASE_NAME                = var.infra-sftp-test-dev.repository.name
 
     DEV_CONTAINER_REGISTRY_SERVICE_CONN = local.service_endpoint_azure_devops_acr_aks_dev_name
     DEV_KUBERNETES_SERVICE_CONN         = local.srv_endpoint_name_aks_dev
@@ -52,30 +58,30 @@ locals {
     # PROD_AGENT_POOL                      = "cstar-prod-linux"
   }
   # deploy secrets
-  sftp-test-variables_secret_deploy = {
+  infra-sftp-test-dev-variables_secret_deploy = {
 
   }
 }
 
-module "sftp-test_code_review" {
+module "infra-sftp-test-dev_code_review" {
   source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_code_review?ref=v2.2.0"
-  count  = var.sftp-test.pipeline.enable_code_review == true ? 1 : 0
+  count  = var.infra-sftp-test-dev.pipeline.enable_code_review == true ? 1 : 0
 
   project_id                   = data.azuredevops_project.project.id
-  repository                   = var.sftp-test.repository
+  repository                   = var.infra-sftp-test-dev.repository
   github_service_connection_id = local.service_endpoint_io_azure_devops_github_pr_id
-  path                         = var.sftp-test.pipeline.path
+  path                         = var.infra-sftp-test-dev.pipeline.path
 
   pull_request_trigger_use_yaml = true
 
   variables = merge(
-    local.sftp-test-variables,
-    local.sftp-test-variables_code_review,
+    local.infra-sftp-test-dev-variables,
+    local.infra-sftp-test-dev-variables_code_review,
   )
 
   variables_secret = merge(
-    local.sftp-test-variables_secret,
-    local.sftp-test-variables_secret_code_review,
+    local.infra-sftp-test-dev-variables_secret,
+    local.infra-sftp-test-dev-variables_secret_code_review,
   )
 
   service_connection_ids_authorization = [
@@ -84,24 +90,24 @@ module "sftp-test_code_review" {
   ]
 }
 
-module "sftp-test_deploy" {
+module "infra-sftp-test-dev_deploy" {
   source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_deploy?ref=v2.2.0"
-  count  = var.sftp-test.pipeline.enable_deploy == true ? 1 : 0
+  count  = var.infra-sftp-test-dev.pipeline.enable_deploy == true ? 1 : 0
 
   project_id                   = data.azuredevops_project.project.id
-  repository                   = var.sftp-test.repository
+  repository                   = var.infra-sftp-test-dev.repository
   github_service_connection_id = local.service_endpoint_io_azure_devops_github_pr_id
-  path                         = var.sftp-test.pipeline.path
+  path                         = var.infra-sftp-test-dev.pipeline.path
   ci_trigger_use_yaml          = true
 
   variables = merge(
-    local.sftp-test-variables,
-    local.sftp-test-variables_deploy,
+    local.infra-sftp-test-dev-variables,
+    local.infra-sftp-test-dev-variables_deploy,
   )
 
   variables_secret = merge(
-    local.sftp-test-variables_secret,
-    local.sftp-test-variables_secret_deploy,
+    local.infra-sftp-test-dev-variables_secret,
+    local.infra-sftp-test-dev-variables_secret_deploy,
   )
 
   service_connection_ids_authorization = [
