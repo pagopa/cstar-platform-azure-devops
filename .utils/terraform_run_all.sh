f0f64f1be72131b653619cec18b3659c9ac71d5a
diegoitaliait <diego.lagosmorales@pagopa.it>
2022-08-03 12:23:02 +0200

    fix terraform_run_all multi threading

--- .utils/terraform_run_all.sh
+++ .utils/terraform_run_all.sh
@@ -13,6 +13,7 @@ set -eu
 
 ACTION="$1"
 MODE="$2"
+pids=()
 
 array=(
     'azure-devops::core'
@@ -35,6 +36,21 @@ for index in "${array[@]}" ; do
         echo "$FOLDER - $COMMAND"
         echo "🔬 folder: $(pwd) in under terraform: $ACTION action $MODE mode"
         sh terraform.sh "$ACTION" "$COMMAND" &
+
+        pids+=($!)
     popd
 done
 
+# Wait for each specific process to terminate.
+# Instead of this loop, a single call to 'wait' would wait for all the jobs
+# to terminate, but it would not give us their exit status.
+#
+for pid in "${pids[@]}"; do
+  #
+  # Waiting on a specific PID makes the wait command return with the exit
+  # status of that process. Because of the 'set -e' setting, any exit status
+  # other than zero causes the current shell to terminate with that exit
+  # status as well.
+  #
+  wait "$pid"
+done
