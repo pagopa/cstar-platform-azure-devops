cff1140cc0f77525ef126bfed70d77f9b4e48082
diegoitaliait <diego.lagosmorales@pagopa.it>
2022-10-10 22:03:34 +0200

    fix tls cert connection

--- azure-devops/idpay-projects/10_tlscert-prod01-idpay-internal-cstar-pagopa-it.tf
+++ azure-devops/idpay-projects/10_tlscert-prod01-idpay-internal-cstar-pagopa-it.tf
@@ -25,19 +25,19 @@ variable "tlscert-prod01-idpay-internal-cstar-pagopa-it" {
 locals {
   tlscert-prod01-idpay-internal-cstar-pagopa-it = {
     tenant_id                           = module.secret_azdo.values["PAGOPAIT-TENANTID"].value
-    subscription_name                   = local.uat_cstar_subscription_name
+    subscription_name                   = local.prod_cstar_subscription_name
     subscription_id                     = module.secret_azdo.values["PAGOPAIT-PROD-CSTAR-SUBSCRIPTION-ID"].value
-    dns_zone_resource_group             = local.rg_uat_dns_zone_name
-    credential_subcription              = local.uat_cstar_subscription_name
-    credential_key_vault_name           = local.uat_domain_key_vault_name
-    credential_key_vault_resource_group = local.uat_domain_key_vault_resource_group
+    dns_zone_resource_group             = local.rg_prod_dns_zone_name
+    credential_subcription              = local.prod_cstar_subscription_name
+    credential_key_vault_name           = local.prod_domain_key_vault_name
+    credential_key_vault_resource_group = local.prod_domain_key_vault_resource_group
     service_connection_ids_authorization = [
       module.PROD-CSTAR-IDPAY-TLS-CERT-SERVICE-CONN.service_endpoint_id,
     ]
   }
   tlscert-prod01-idpay-internal-cstar-pagopa-it-variables = {
     KEY_VAULT_SERVICE_CONNECTION = module.PROD-CSTAR-IDPAY-TLS-CERT-SERVICE-CONN.service_endpoint_name,
-    KEY_VAULT_NAME               = local.uat_domain_key_vault_name
+    KEY_VAULT_NAME               = local.prod_domain_key_vault_name
   }
   tlscert-prod01-idpay-internal-cstar-pagopa-it-variables_secret = {
   }
@@ -51,7 +51,7 @@ module "tlscert-prod01-idpay-internal-cstar-pagopa-it-cert_az" {
 
   # change me
   providers = {
-    azurerm = azurerm.uat
+    azurerm = azurerm.prod
   }
 
   project_id = data.azuredevops_project.project.id
@@ -99,6 +99,6 @@ module "tlscert-prod01-idpay-internal-cstar-pagopa-it-cert_az" {
   }
 
   depends_on = [
-    module.letsencrypt_uat
+    module.letsencrypt_prod
   ]
 }
