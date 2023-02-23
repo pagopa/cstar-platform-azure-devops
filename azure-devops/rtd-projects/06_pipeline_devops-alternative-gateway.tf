61515be3fc9c4589d9c5af330a4e24a0926e82e9
Andrea Morabito <andrea.morabito@pagopa.it>
2023-02-23 10:48:24 +0100

    [RTD-1380] fix pipeline config

--- azure-devops/rtd-projects/06_pipeline_devops-alternative-gateway.tf
+++ azure-devops/rtd-projects/06_pipeline_devops-alternative-gateway.tf
@@ -110,13 +110,5 @@ module "rtd-ms-alternative-gateway_deploy" {
     local.service_endpoint_azure_devops_docker_dev_id,
     azuredevops_serviceendpoint_kubernetes.aks_dev.id,
 
-
-    local.service_endpoint_azure_uat_id,
-    local.service_endpoint_azure_devops_docker_uat_id,
-    azuredevops_serviceendpoint_kubernetes.aks_uat.id,
-
-    local.service_endpoint_azure_prod_id,
-    local.service_endpoint_azure_devops_docker_prod_id,
-    azuredevops_serviceendpoint_kubernetes.aks_prod.id
   ]
 }
