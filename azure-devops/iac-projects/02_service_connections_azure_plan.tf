4eec1f74ca2580b5cb144087833197d75d8260c2
diegoitaliait <diego.lagosmorales@pagopa.it>
2023-06-21 14:54:46 +0200

    fix module version

--- azure-devops/iac-projects/02_service_connections_azure_plan.tf
+++ azure-devops/iac-projects/02_service_connections_azure_plan.tf
@@ -8,7 +8,7 @@ module "DEV-CSTAR-PLAN-SERVICE-CONN" {
   }
 
   depends_on = [data.azuredevops_project.project]
-  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_azurerm_plan?ref=fix-azure-plan"
+  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_azurerm_plan?ref=v3.1.1"
 
   name_suffix                 = "${local.project_prefix_short}-dev"
   iac_aad_group_name          = "azure-devops-iac-service-connection"
@@ -34,7 +34,7 @@ module "UAT-CSTAR-PLAN-SERVICE-CONN" {
   }
 
   depends_on = [data.azuredevops_project.project]
-  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_azurerm_plan?ref=fix-azure-plan"
+  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_azurerm_plan?ref=v3.1.1"
 
   name_suffix                 = "${local.project_prefix_short}-uat"
   iac_aad_group_name          = "azure-devops-iac-service-connection"
@@ -60,7 +60,7 @@ module "PROD-CSTAR-PLAN-SERVICE-CONN" {
   }
 
   depends_on = [data.azuredevops_project.project]
-  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_azurerm_plan?ref=fix-azure-plan"
+  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_azurerm_plan?ref=v3.1.1"
 
   name_suffix                 = "${local.project_prefix_short}-prod"
   iac_aad_group_name          = "azure-devops-iac-service-connection"
