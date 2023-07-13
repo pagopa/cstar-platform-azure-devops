5b4a9b4f35583be15cdb58d58ea15bbc51fb6e1c
TommasoLencioni <tommaso.lencioni@pagopa.it>
2023-07-13 17:23:20 +0200

    [IDP-1311] add refund flow testing pipeline

--- azure-devops/idpay-projects/06_pipeline_idpay-functional-testing.tf
+++ azure-devops/idpay-projects/06_pipeline_idpay-functional-testing.tf
@@ -101,3 +101,31 @@ module "idpay-functional-testing_scheduled_test_discount" {
     local.service_endpoint_io_azure_devops_github_ro_id,
   ]
 }
+
+module "idpay-functional-testing_scheduled_test_refund" {
+  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_generic?ref=v2.7.0"
+  count  = var.idpay-functional-testing.pipeline.enable_scheduled_test == true ? 1 : 0
+
+  pipeline_name                = "${var.idpay-functional-testing.repository.yml_prefix_name}.${var.idpay-functional-testing-refund-flow.flow_name}"
+  pipeline_yml_filename        = "${var.idpay-functional-testing.repository.yml_prefix_name}-${var.idpay-functional-testing-refund-flow.flow_name}${var.idpay-functional-testing.repository.yml_suffix_name}"
+  project_id                   = data.azuredevops_project.project.id
+  repository                   = var.idpay-functional-testing.repository
+  github_service_connection_id = local.service_endpoint_io_azure_devops_github_pr_id
+  path                         = var.idpay-functional-testing.pipeline.path
+
+  pull_request_trigger_use_yaml = true
+
+  variables = merge(
+    local.idpay-functional-testing-variables,
+    local.idpay-functional-testing-variables_scheduled_test,
+  )
+
+  variables_secret = merge(
+    local.idpay-functional-testing-variables_secret,
+    local.idpay-functional-testing-secret_scheduled_test,
+  )
+
+  service_connection_ids_authorization = [
+    local.service_endpoint_io_azure_devops_github_ro_id,
+  ]
+}
