19c4346add2b27dd40bd74ed85b85bdd563c58d2
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-01-29 12:09:32 +0100

    removed azurerm_limited moduels

--- azure-devops/rtd-projects/03_service_connection_tls_certificate.tf
+++ azure-devops/rtd-projects/03_service_connection_tls_certificate.tf
@@ -1,28 +1,6 @@
 #
 # DEV
 #
-module "DEV-CSTAR-RTD-TLS-CERT-SERVICE-CONN" {
-
-  providers = {
-    azurerm = azurerm.dev
-  }
-
-  depends_on = [data.azuredevops_project.project]
-  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_azurerm_limited?ref=v5.2.0"
-
-  project_id        = data.azuredevops_project.project.id
-  name              = "${local.prefix}-d-${local.domain}-tls-cert"
-  tenant_id         = data.azurerm_client_config.current.tenant_id
-  subscription_name = local.dev_cstar_subscription_name
-  subscription_id   = data.azurerm_subscriptions.dev.subscriptions[0].subscription_id
-  #tfsec:ignore:GEN003
-  renew_token = local.tlscert_renew_token
-
-  credential_subcription              = local.dev_cstar_subscription_name
-  credential_key_vault_name           = local.dev_domain_key_vault_name
-  credential_key_vault_resource_group = local.dev_domain_key_vault_resource_group
-}
-
 module "DEV-CSTAR-RTD-TLS-CERT-SERVICE-CONN-FEDERATED" {
 
   providers = {
@@ -68,28 +46,6 @@ module "letsencrypt_dev" {
 #
 # UAT
 #
-module "UAT-CSTAR-RTD-TLS-CERT-SERVICE-CONN" {
-
-  providers = {
-    azurerm = azurerm.uat
-  }
-
-  depends_on = [data.azuredevops_project.project]
-  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_azurerm_limited?ref=v5.2.0"
-
-  project_id        = data.azuredevops_project.project.id
-  name              = "${local.prefix}-u-${local.domain}-tls-cert"
-  tenant_id         = data.azurerm_client_config.current.tenant_id
-  subscription_name = local.uat_cstar_subscription_name
-  subscription_id   = data.azurerm_subscriptions.uat.subscriptions[0].subscription_id
-  #tfsec:ignore:GEN003
-  renew_token = local.tlscert_renew_token
-
-  credential_subcription              = local.uat_cstar_subscription_name
-  credential_key_vault_name           = local.uat_domain_key_vault_name
-  credential_key_vault_resource_group = local.uat_domain_key_vault_resource_group
-}
-
 module "UAT-CSTAR-RTD-TLS-CERT-SERVICE-CONN-FEDERATED" {
 
   providers = {
@@ -135,28 +91,6 @@ module "letsencrypt_uat" {
 #
 # PROD
 #
-module "PROD-CSTAR-RTD-TLS-CERT-SERVICE-CONN" {
-
-  providers = {
-    azurerm = azurerm.prod
-  }
-
-  depends_on = [data.azuredevops_project.project]
-  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_azurerm_limited?ref=v5.2.0"
-
-  project_id        = data.azuredevops_project.project.id
-  name              = "${local.prefix}-p-${local.domain}-tls-cert"
-  tenant_id         = data.azurerm_client_config.current.tenant_id
-  subscription_name = local.prod_cstar_subscription_name
-  subscription_id   = data.azurerm_subscriptions.prod.subscriptions[0].subscription_id
-  #tfsec:ignore:GEN003
-  renew_token = local.tlscert_renew_token
-
-  credential_subcription              = local.prod_cstar_subscription_name
-  credential_key_vault_name           = local.prod_domain_key_vault_name
-  credential_key_vault_resource_group = local.prod_domain_key_vault_resource_group
-}
-
 module "PROD-CSTAR-RTD-TLS-CERT-SERVICE-CONN-FEDERATED" {
 
   providers = {
