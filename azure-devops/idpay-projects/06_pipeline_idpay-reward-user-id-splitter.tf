72a52d2fdf43aa3a7e11f30f06c9846848b863c7
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-01-29 11:04:28 +0100

    fix sonar service connection id

--- azure-devops/idpay-projects/06_pipeline_idpay-reward-user-id-splitter.tf
+++ azure-devops/idpay-projects/06_pipeline_idpay-reward-user-id-splitter.tf
@@ -86,7 +86,7 @@ module "idpay-reward-user-id-splitter_code_review" {
 
   service_connection_ids_authorization = [
     local.service_endpoint_io_azure_devops_github_ro_id,
-    local.azuredevops_serviceendpoint_sonarcloud_id,
+    data.azuredevops_serviceendpoint_sonarcloud.sonar_cloud.id,
   ]
 }
 
