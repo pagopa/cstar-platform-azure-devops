9e1b84973856f679029c98c4879aadc3fbd077e3
mamari90 <130982006+mamari90@users.noreply.github.com>
2024-08-01 12:05:03 +0200

    added mil deploy pipeline

--- azure-devops/mil-projects/00_service_endpoint_docker.tf
+++ azure-devops/mil-projects/00_service_endpoint_docker.tf
@@ -4,11 +4,11 @@ data "azuredevops_serviceendpoint_azurecr" "dev-azureacr-service-endpoint" {
   service_endpoint_name = local.service_endpoint_azure_devops_docker_dev_name
 }
 
-# data "azuredevops_serviceendpoint_azurecr" "uat-azureacr-service-endpoint" {
-#   project_id            = data.azuredevops_project.project.id
-#   service_endpoint_name = local.service_endpoint_azure_devops_docker_uat_name
-# }
-#
+ data "azuredevops_serviceendpoint_azurecr" "uat-azureacr-service-endpoint" {
+   project_id            = data.azuredevops_project.project.id
+   service_endpoint_name = local.service_endpoint_azure_devops_docker_uat_name
+ }
+
 # data "azuredevops_serviceendpoint_azurecr" "prod-azureacr-service-endpoint" {
 #   project_id            = data.azuredevops_project.project.id
 #   service_endpoint_name = local.service_endpoint_azure_devops_docker_prod_name
