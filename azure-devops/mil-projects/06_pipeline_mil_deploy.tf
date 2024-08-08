2360e9a97f4eb66a17770bfa7e87b89b70f79dd7
Luca Consalvi <luca.consalvi@pagopa.it>
2024-08-08 15:46:20 +0200

    precommit run

--- azure-devops/mil-projects/06_pipeline_mil_deploy.tf
+++ azure-devops/mil-projects/06_pipeline_mil_deploy.tf
@@ -32,16 +32,16 @@ locals {
     SETTINGS_XML_RO_SECURE_FILE_NAME = "settings-ro.xml"
     HELM_RELEASE_NAME                = var.mil-deploy.repository.name
 
-    DEV_CONTAINER_REGISTRY_SERVICE_CONN  = local.service_endpoint_azure_devops_docker_dev_name
-    DEV_KUBERNETES_SERVICE_CONN          = local.srv_endpoint_name_aks_dev
-    DEV_CONTAINER_REGISTRY_NAME          = local.aks_dev_docker_registry_name
-    DEV_AGENT_POOL                       = local.azdo_agent_pool_dev
-    UAT_KUBERNETES_SERVICE_CONN          = local.srv_endpoint_name_aks_uat
-    UAT_CONTAINER_REGISTRY_NAME          = local.aks_uat_docker_registry_name
-    UAT_AGENT_POOL                       = local.azdo_agent_pool_uat
-    PROD_KUBERNETES_SERVICE_CONN         = local.srv_endpoint_name_aks_prod
-    PROD_CONTAINER_REGISTRY_NAME         = local.aks_prod_docker_registry_name
-    PROD_AGENT_POOL                      = local.azdo_agent_pool_prod
+    DEV_CONTAINER_REGISTRY_SERVICE_CONN = local.service_endpoint_azure_devops_docker_dev_name
+    DEV_KUBERNETES_SERVICE_CONN         = local.srv_endpoint_name_aks_dev
+    DEV_CONTAINER_REGISTRY_NAME         = local.aks_dev_docker_registry_name
+    DEV_AGENT_POOL                      = local.azdo_agent_pool_dev
+    UAT_KUBERNETES_SERVICE_CONN         = local.srv_endpoint_name_aks_uat
+    UAT_CONTAINER_REGISTRY_NAME         = local.aks_uat_docker_registry_name
+    UAT_AGENT_POOL                      = local.azdo_agent_pool_uat
+    PROD_KUBERNETES_SERVICE_CONN        = local.srv_endpoint_name_aks_prod
+    PROD_CONTAINER_REGISTRY_NAME        = local.aks_prod_docker_registry_name
+    PROD_AGENT_POOL                     = local.azdo_agent_pool_prod
   }
   # deploy secrets
   mil-deploy-variables_secret_deploy = {
@@ -83,9 +83,9 @@ module "mil-deploy_deploy" {
     local.service_endpoint_azure_devops_docker_uat_id,
     azuredevops_serviceendpoint_kubernetes.aks_uat.id,
     #prod
-#    local.service_endpoint_azure_prod_id,
-#    local.service_endpoint_azure_devops_docker_prod_id,
-#    azuredevops_serviceendpoint_kubernetes.aks_prod.id,
+    #    local.service_endpoint_azure_prod_id,
+    #    local.service_endpoint_azure_devops_docker_prod_id,
+    #    azuredevops_serviceendpoint_kubernetes.aks_prod.id,
   ]
 
 }
