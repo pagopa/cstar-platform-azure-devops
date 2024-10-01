ced8f729baaf804e7bca4949457d488c3565e971
Luca Consalvi <luca.consalvi@pagopa.it>
2024-10-01 15:16:10 +0200

    add iac rtp and variables

--- azure-devops/iac-projects/99_variables.tf
+++ azure-devops/iac-projects/99_variables.tf
@@ -35,6 +35,16 @@ locals {
   dev_tae_key_vault_name  = "${local.prefix}-d-tae-kv"
   uat_tae_key_vault_name  = "${local.prefix}-u-tae-kv"
   prod_tae_key_vault_name = "${local.prefix}-p-tae-kv"
+
+  # 🔐 RTP KV Domain
+  dev_rtp_key_vault_resource_group  = "${local.prefix}-d-rtp-sec-rg"
+  uat_rtp_key_vault_resource_group  = "${local.prefix}-u-rtp-sec-rg"
+  prod_rtp_key_vault_resource_group = "${local.prefix}-p-rtp-sec-rg"
+
+  dev_rtp_key_vault_name  = "${local.prefix}-d-rtp-kv"
+  uat_rtp_key_vault_name  = "${local.prefix}-u-rtp-kv"
+  prod_rtp_key_vault_name = "${local.prefix}-p-rtp-kv"
+
 }
 
 variable "dev_subscription_name" {
