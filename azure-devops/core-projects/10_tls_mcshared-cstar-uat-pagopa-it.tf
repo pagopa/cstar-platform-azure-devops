7f66a5cb8604179b87d5be79204a51a596107163
Luca Consalvi <luca.consalvi@pagopa.it>
2024-11-14 15:45:11 +0100

    setup certificate for mcshared listener

---
+++ azure-devops/core-projects/10_tls_mcshared-cstar-uat-pagopa-it.tf
@@ -0,0 +1,102 @@
+variable "mcshared-cstar-uat-pagopa-it" {
+  default = {
+    repository = {
+      organization   = "pagopa"
+      name           = "le-azure-acme-tiny"
+      branch_name    = "refs/heads/master"
+      pipelines_path = "."
+    }
+    pipeline = {
+      enable_tls_cert = true
+      path            = "TLS-Certificates\\UAT"
+      dns_record_name = "api-mcshared"
+      dns_zone_name   = "uat.cstar.pagopa.it"
+      # common variables to all pipelines
+      variables = {
+        CERT_NAME_EXPIRE_SECONDS = "2592000" #30 days
+      }
+      # common secret variables to all pipelines
+      variables_secret = {
+      }
+    }
+  }
+}
+
+locals {
+  mcshared-cstar-uat-pagopa-it = {
+    tenant_id                           = data.azurerm_client_config.current.tenant_id
+    subscription_name                   = local.uat_subscription_name
+    subscription_id                     = data.azurerm_subscriptions.uat.subscriptions[0].subscription_id
+    dns_zone_resource_group             = local.rg_uat_dns_zone_name
+    credential_subcription              = local.uat_subscription_name
+    credential_key_vault_name           = local.uat_domain_key_vault_name
+    credential_key_vault_resource_group = local.uat_domain_key_vault_resource_group
+    service_connection_ids_authorization = [
+      module.UAT-CSTAR-CORE-TLS-CERT-SERVICE-CONN-FEDERATED.service_endpoint_id,
+    ]
+  }
+  mcshared-cstar-uat-pagopa-it-variables = {
+    KEY_VAULT_SERVICE_CONNECTION = module.UAT-CSTAR-CORE-TLS-CERT-SERVICE-CONN-FEDERATED.service_endpoint_name,
+    KEY_VAULT_NAME               = local.uat_domain_key_vault_name
+  }
+  mcshared-cstar-uat-pagopa-it-variables_secret = {
+  }
+}
+
+# change only providers
+#tfsec:ignore:general-secrets-no-plaintext-exposure
+module "mcshared-cstar-uat-pagopa-it-cert_az" {
+  source = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_build_definition_tls_cert_federated?ref=v5.2.0"
+  count  = var.mcshared-cstar-uat-pagopa-it.pipeline.enable_tls_cert == true ? 1 : 0
+
+  # change me
+  providers = {
+    azurerm = azurerm.uat
+  }
+
+  location                             = local.location
+  managed_identity_resource_group_name = local.uat_identity_rg_name
+
+  project_id                   = data.azuredevops_project.project.id
+  repository                   = var.mcshared-cstar-uat-pagopa-it.repository
+  path                         = "${local.domain}\\${var.mcshared-cstar-uat-pagopa-it.pipeline.path}"
+  github_service_connection_id = azuredevops_serviceendpoint_github.io-azure-devops-github-rw.id
+
+  dns_record_name         = var.mcshared-cstar-uat-pagopa-it.pipeline.dns_record_name
+  dns_zone_name           = var.mcshared-cstar-uat-pagopa-it.pipeline.dns_zone_name
+  dns_zone_resource_group = local.mcshared-cstar-uat-pagopa-it.dns_zone_resource_group
+  tenant_id               = local.mcshared-cstar-uat-pagopa-it.tenant_id
+  subscription_name       = local.mcshared-cstar-uat-pagopa-it.subscription_name
+  subscription_id         = local.mcshared-cstar-uat-pagopa-it.subscription_id
+
+  credential_key_vault_name           = local.mcshared-cstar-uat-pagopa-it.credential_key_vault_name
+  credential_key_vault_resource_group = local.mcshared-cstar-uat-pagopa-it.credential_key_vault_resource_group
+
+  variables = merge(
+    var.mcshared-cstar-uat-pagopa-it.pipeline.variables,
+    local.mcshared-cstar-uat-pagopa-it-variables,
+  )
+
+  variables_secret = merge(
+    var.mcshared-cstar-uat-pagopa-it.pipeline.variables_secret,
+    local.mcshared-cstar-uat-pagopa-it-variables_secret,
+  )
+
+  service_connection_ids_authorization = local.mcshared-cstar-uat-pagopa-it.service_connection_ids_authorization
+
+  schedules = {
+    days_to_build              = ["Fri"]
+    schedule_only_with_changes = false
+    start_hours                = 3
+    start_minutes              = 0
+    time_zone                  = "(UTC+01:00) Amsterdam, Berlin, Bern, Rome, Stockholm, Vienna"
+    branch_filter = {
+      include = [var.mcshared-cstar-uat-pagopa-it.repository.branch_name]
+      exclude = []
+    }
+  }
+
+  depends_on = [
+    module.letsencrypt_uat
+  ]
+}
