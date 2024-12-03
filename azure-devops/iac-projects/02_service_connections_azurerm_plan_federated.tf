33d89048491620daaee2c2d2d58745273b7cfbdd
diegoitaliait <diego.lagosmorales@pagopa.it>
2024-12-03 11:24:55 +0100

    alpha service connections azurerm with federated credentials

---
+++ azure-devops/iac-projects/02_service_connections_azurerm_plan_federated.tf
@@ -0,0 +1,90 @@
+#
+# ⛩ Service connections
+#
+
+module "DEV-AZURERM-IAC-PLAN-SERVICE-CONN" {
+  depends_on = [data.azuredevops_project.project]
+  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_federated?ref=v9.2.1"
+  providers = {
+    azurerm = azurerm.dev
+  }
+
+  project_id = data.azuredevops_project.project.id
+  #tfsec:ignore:general-secrets-no-plaintext-exposure
+  name = "azdo-dev-p4pa-iac-plan"
+
+  tenant_id         = data.azurerm_client_config.current.tenant_id
+  subscription_id   = data.azurerm_subscriptions.dev.subscriptions[0].subscription_id
+  subscription_name = local.dev_subscription_name
+
+  location            = var.location
+  resource_group_name = local.dev_identity_rg_name
+}
+
+
+resource "azurerm_role_assignment" "dev_plan_permissions" {
+
+  scope                = data.azurerm_subscriptions.dev.subscriptions[0].id
+  role_definition_name = "P4pa Platform Dev IaC Reader"
+  principal_id         = module.DEV-AZURERM-IAC-PLAN-SERVICE-CONN.identity_principal_id
+}
+
+#
+# UAT
+#
+
+module "UAT-AZURERM-IAC-PLAN-SERVICE-CONN" {
+  depends_on = [data.azuredevops_project.project]
+  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_federated?ref=v9.2.1"
+  providers = {
+    azurerm = azurerm.uat
+  }
+
+  project_id = data.azuredevops_project.project.id
+  #tfsec:ignore:general-secrets-no-plaintext-exposure
+  name = "azdo-uat-p4pa-iac-plan"
+
+  tenant_id         = data.azurerm_client_config.current.tenant_id
+  subscription_id   = data.azurerm_subscriptions.uat.subscriptions[0].subscription_id
+  subscription_name = local.uat_subscription_name
+
+  location            = var.location
+  resource_group_name = local.uat_identity_rg_name
+}
+
+resource "azurerm_role_assignment" "uat_plan_permissions" {
+
+  scope                = data.azurerm_subscriptions.uat.subscriptions[0].id
+  role_definition_name = "P4pa Platform Uat IaC Reader"
+  principal_id         = module.UAT-AZURERM-IAC-PLAN-SERVICE-CONN.identity_principal_id
+}
+
+#
+# PROD
+#
+
+module "PROD-AZURERM-IAC-PLAN-SERVICE-CONN" {
+  depends_on = [data.azuredevops_project.project]
+  source     = "git::https://github.com/pagopa/azuredevops-tf-modules.git//azuredevops_serviceendpoint_federated?ref=v9.2.1"
+  providers = {
+    azurerm = azurerm.prod
+  }
+
+  project_id = data.azuredevops_project.project.id
+  #tfsec:ignore:general-secrets-no-plaintext-exposure
+  name = "azdo-prod-p4pa-iac-plan"
+
+  tenant_id         = data.azurerm_client_config.current.tenant_id
+  subscription_id   = data.azurerm_subscriptions.prod.subscriptions[0].subscription_id
+  subscription_name = local.prod_subscription_name
+
+  location            = var.location
+  resource_group_name = local.prod_identity_rg_name
+}
+
+resource "azurerm_role_assignment" "prod_plan_permissions" {
+
+  scope                = data.azurerm_subscriptions.prod.subscriptions[0].id
+  role_definition_name = "P4pa Platform Prod IaC Reader"
+  principal_id         = module.PROD-AZURERM-IAC-PLAN-SERVICE-CONN.identity_principal_id
+}
