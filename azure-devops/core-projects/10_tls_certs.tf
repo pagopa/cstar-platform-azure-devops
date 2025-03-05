8e4371c8668dcc7a7e69663eab73837b07db8f5e
mamari90 <130982006+mamari90@users.noreply.github.com>
2025-03-05 15:12:41 +0100

    precommit

--- azure-devops/core-projects/10_tls_certs.tf
+++ azure-devops/core-projects/10_tls_certs.tf
@@ -2,73 +2,73 @@ locals {
 
   env_mappings = {
     dev = {
-      dns_zone_name = "dev.cstar.pagopa.it"
-      dns_zone_rg = local.rg_dev_dns_zone_name
-      subscription_name = local.dev_subscription_name
-      subscription_id = local.dev_subscription_id
+      dns_zone_name                       = "dev.cstar.pagopa.it"
+      dns_zone_rg                         = local.rg_dev_dns_zone_name
+      subscription_name                   = local.dev_subscription_name
+      subscription_id                     = local.dev_subscription_id
       credential_key_vault_name           = local.dev_domain_key_vault_name
       credential_key_vault_resource_group = local.dev_domain_key_vault_resource_group
-      service_endpoint = module.dev_cstar_core_tls_cert_service_conn_federated.service_endpoint_id
+      service_endpoint                    = module.dev_cstar_core_tls_cert_service_conn_federated.service_endpoint_id
       variables = {
         KEY_VAULT_SERVICE_CONNECTION = module.dev_cstar_core_tls_cert_service_conn_federated.service_endpoint_name
         KEY_VAULT_NAME               = local.dev_domain_key_vault_name
-        CERT_NAME_EXPIRE_SECONDS = "2592000"
+        CERT_NAME_EXPIRE_SECONDS     = "2592000"
       }
       variables_secret = {}
     }
     uat = {
-      dns_zone_name = "uat.cstar.pagopa.it"
-      dns_zone_rg = local.rg_uat_dns_zone_name
-      subscription_name = local.uat_subscription_name
-      subscription_id = local.uat_subscription_id
+      dns_zone_name                       = "uat.cstar.pagopa.it"
+      dns_zone_rg                         = local.rg_uat_dns_zone_name
+      subscription_name                   = local.uat_subscription_name
+      subscription_id                     = local.uat_subscription_id
       credential_key_vault_name           = local.uat_domain_key_vault_name
       credential_key_vault_resource_group = local.uat_domain_key_vault_resource_group
-      service_endpoint = module.uat_cstar_core_tls_cert_service_conn_federated.service_endpoint_id
+      service_endpoint                    = module.uat_cstar_core_tls_cert_service_conn_federated.service_endpoint_id
       variables = {
         KEY_VAULT_SERVICE_CONNECTION = module.uat_cstar_core_tls_cert_service_conn_federated.service_endpoint_name
         KEY_VAULT_NAME               = local.uat_domain_key_vault_name
-        CERT_NAME_EXPIRE_SECONDS = "2592000"
+        CERT_NAME_EXPIRE_SECONDS     = "2592000"
       }
       variables_secret = {}
     }
     prod = {
-      dns_zone_name = "cstar.pagopa.it"
-      dns_zone_rg = local.rg_prod_dns_zone_name
-      subscription_name = local.prod_subscription_name
-      subscription_id = local.prod_subscription_id
+      dns_zone_name                       = "cstar.pagopa.it"
+      dns_zone_rg                         = local.rg_prod_dns_zone_name
+      subscription_name                   = local.prod_subscription_name
+      subscription_id                     = local.prod_subscription_id
       credential_key_vault_name           = local.prod_domain_key_vault_name
       credential_key_vault_resource_group = local.prod_domain_key_vault_resource_group
-      service_endpoint = module.prod_cstar_core_tls_cert_service_conn_federated.service_endpoint_id
+      service_endpoint                    = module.prod_cstar_core_tls_cert_service_conn_federated.service_endpoint_id
       variables = {
         KEY_VAULT_SERVICE_CONNECTION = module.prod_cstar_core_tls_cert_service_conn_federated.service_endpoint_name
         KEY_VAULT_NAME               = local.prod_domain_key_vault_name
-        CERT_NAME_EXPIRE_SECONDS = "2592000"
+        CERT_NAME_EXPIRE_SECONDS     = "2592000"
       }
       variables_secret = {}
     }
   }
 
 
-certificates = {
-  "rtp-cb-cstar-dev-pagopa-it": {
-    env = "dev"
-    dns_record_name = "api-rtp-cb"
-    variables = {}
-    variables_secret = {}
+  certificates = {
+    "rtp-cb-cstar-dev-pagopa-it" : {
+      env              = "dev"
+      dns_record_name  = "api-rtp-cb"
+      variables        = {}
+      variables_secret = {}
+    }
+    "rtp-cb-cstar-pagopa-it" : {
+      env              = "prod"
+      dns_record_name  = "api-rtp-cb"
+      variables        = {}
+      variables_secret = {}
+    }
+    "rtp-cb-cstar-uat-pagopa-it" : {
+      env              = "uat"
+      dns_record_name  = "api-rtp-cb"
+      variables        = {}
+      variables_secret = {}
+    }
   }
-  "rtp-cb-cstar-pagopa-it": {
-    env = "prod"
-    dns_record_name = "api-rtp-cb"
-    variables = {}
-    variables_secret = {}
-  }
-  "rtp-cb-cstar-uat-pagopa-it": {
-    env = "uat"
-    dns_record_name = "api-rtp-cb"
-    variables = {}
-    variables_secret = {}
-  }
-}
 
 
 
@@ -81,7 +81,7 @@ certificates = {
 module "federated_cert_pipeline_dev" {
   source = "./.terraform/modules/__devops_v0__/azuredevops_build_definition_tls_cert_federated"
 
-  for_each = {for k,v in local.certificates: k => v if v.env == "dev"}
+  for_each = { for k, v in local.certificates : k => v if v.env == "dev" }
 
   providers = {
     azurerm = azurerm.dev
@@ -115,7 +115,7 @@ module "federated_cert_pipeline_dev" {
     each.value.variables_secret
   )
 
-  service_connection_ids_authorization = [ local.env_mappings[each.value.env].service_endpoint ]
+  service_connection_ids_authorization = [local.env_mappings[each.value.env].service_endpoint]
 
   schedules = {
     days_to_build              = ["Fri"]
@@ -141,14 +141,14 @@ module "federated_cert_pipeline_dev" {
 module "federated_cert_pipeline_uat" {
   source = "./.terraform/modules/__devops_v0__/azuredevops_build_definition_tls_cert_federated"
 
-  for_each = {for k,v in local.certificates: k => v if v.env == "uat"}
+  for_each = { for k, v in local.certificates : k => v if v.env == "uat" }
 
   providers = {
     azurerm = azurerm.uat
   }
 
   location                             = local.location
-  managed_identity_resource_group_name = local.dev_identity_rg_name
+  managed_identity_resource_group_name = local.uat_identity_rg_name
 
   project_id                   = data.azuredevops_project.project.id
   repository                   = local.tlscert_repository
@@ -175,7 +175,7 @@ module "federated_cert_pipeline_uat" {
     each.value.variables_secret
   )
 
-  service_connection_ids_authorization = [ local.env_mappings[each.value.env].service_endpoint ]
+  service_connection_ids_authorization = [local.env_mappings[each.value.env].service_endpoint]
 
   schedules = {
     days_to_build              = ["Fri"]
@@ -201,14 +201,14 @@ module "federated_cert_pipeline_uat" {
 module "federated_cert_pipeline_prod" {
   source = "./.terraform/modules/__devops_v0__/azuredevops_build_definition_tls_cert_federated"
 
-  for_each = {for k,v in local.certificates: k => v if v.env == "prod"}
+  for_each = { for k, v in local.certificates : k => v if v.env == "prod" }
 
   providers = {
     azurerm = azurerm.prod
   }
 
   location                             = local.location
-  managed_identity_resource_group_name = local.dev_identity_rg_name
+  managed_identity_resource_group_name = local.prod_identity_rg_name
 
   project_id                   = data.azuredevops_project.project.id
   repository                   = local.tlscert_repository
@@ -235,7 +235,7 @@ module "federated_cert_pipeline_prod" {
     each.value.variables_secret
   )
 
-  service_connection_ids_authorization = [ local.env_mappings[each.value.env].service_endpoint ]
+  service_connection_ids_authorization = [local.env_mappings[each.value.env].service_endpoint]
 
   schedules = {
     days_to_build              = ["Fri"]
