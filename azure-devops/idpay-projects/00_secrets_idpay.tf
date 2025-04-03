7ddeea592f969655886064d202fd5af1ea33bcbc
and-mora <andrea.morabito@pagopa.it>
2025-04-03 10:39:33 +0200

    add missing secrets retrieval

--- azure-devops/idpay-projects/00_secrets_idpay.tf
+++ azure-devops/idpay-projects/00_secrets_idpay.tf
@@ -17,6 +17,9 @@ module "domain_dev_secrets" {
     "cstar-d-weu-dev01-aks-apiserver-url",
     "cstar-d-weu-dev01-aks-azure-devops-sa-token",
     "cstar-d-weu-dev01-aks-azure-devops-sa-cacrt",
+    "argocd-admin-password",
+    "argocd-admin-username",
+    "argocd-server-url",
   ]
 }
 
@@ -33,7 +36,10 @@ module "domain_uat_secrets" {
   secrets = [
     "cstar-u-weu-uat01-aks-azure-devops-sa-token",
     "cstar-u-weu-uat01-aks-azure-devops-sa-cacrt",
-    "cstar-u-weu-uat01-aks-apiserver-url"
+    "cstar-u-weu-uat01-aks-apiserver-url",
+    "argocd-admin-password",
+    "argocd-admin-username",
+    "argocd-server-url",
   ]
 }
 
@@ -50,6 +56,9 @@ module "domain_prod_secrets" {
   secrets = [
     "cstar-p-weu-prod01-aks-azure-devops-sa-token",
     "cstar-p-weu-prod01-aks-azure-devops-sa-cacrt",
-    "cstar-p-weu-prod01-aks-apiserver-url"
+    "cstar-p-weu-prod01-aks-apiserver-url",
+    "argocd-admin-password",
+    "argocd-admin-username",
+    "argocd-server-url",
   ]
 }
