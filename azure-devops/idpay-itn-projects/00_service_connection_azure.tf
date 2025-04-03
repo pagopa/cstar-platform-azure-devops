dfdeeb8e94ad0af67f8a57380ffaacdd3e6a3480
and-mora <andrea.morabito@pagopa.it>
2025-04-03 11:21:13 +0200

    refactor variables

--- azure-devops/idpay-itn-projects/00_service_connection_azure.tf
+++ azure-devops/idpay-itn-projects/00_service_connection_azure.tf
@@ -1,14 +1,14 @@
 data "azuredevops_serviceendpoint_azurerm" "azure_dev" {
-  project_id            = data.azuredevops_project.project.id
-  service_endpoint_name = var.service_endpoint_azure_dev_name
+  project_id            = data.azuredevops_project.this.id
+  service_endpoint_name = "${data.azurerm_subscriptions.dev.subscriptions[0].display_name}-AZURE-SERVICE-CONN"
 }
 
 data "azuredevops_serviceendpoint_azurerm" "azure_uat" {
-  project_id            = data.azuredevops_project.project.id
-  service_endpoint_name = var.service_endpoint_azure_uat_name
+  project_id            = data.azuredevops_project.this.id
+  service_endpoint_name = "${data.azurerm_subscriptions.uat.subscriptions[0].display_name}-AZURE-SERVICE-CONN"
 }
 
 data "azuredevops_serviceendpoint_azurerm" "azure_prod" {
-  project_id            = data.azuredevops_project.project.id
-  service_endpoint_name = var.service_endpoint_azure_prod_name
+  project_id            = data.azuredevops_project.this.id
+  service_endpoint_name = "${data.azurerm_subscriptions.prod.subscriptions[0].display_name}-AZURE-SERVICE-CONN"
 }
