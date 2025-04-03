7f0337a027d16166806dacd7bd348f2d0292d363
and-mora <andrea.morabito@pagopa.it>
2025-04-03 11:27:21 +0200

    add service connections

--- azure-devops/idpay-itn-projects/00_service_connection.tf
+++ azure-devops/idpay-itn-projects/00_service_connection.tf
@@ -1,14 +1,27 @@
 data "azuredevops_serviceendpoint_azurerm" "azure_dev" {
-  project_id            = data.azuredevops_project.this.id
+  project_id            = local.devops_project_id
   service_endpoint_name = "${data.azurerm_subscriptions.dev.subscriptions[0].display_name}-AZURE-SERVICE-CONN"
 }
 
 data "azuredevops_serviceendpoint_azurerm" "azure_uat" {
-  project_id            = data.azuredevops_project.this.id
+  project_id            = local.devops_project_id
   service_endpoint_name = "${data.azurerm_subscriptions.uat.subscriptions[0].display_name}-AZURE-SERVICE-CONN"
 }
 
 data "azuredevops_serviceendpoint_azurerm" "azure_prod" {
-  project_id            = data.azuredevops_project.this.id
+  project_id            = local.devops_project_id
   service_endpoint_name = "${data.azurerm_subscriptions.prod.subscriptions[0].display_name}-AZURE-SERVICE-CONN"
+}
+
+#
+# GITHUB
+#
+data "azuredevops_serviceendpoint_github" "azure_devops_github_rw" {
+  project_id            = local.devops_project_id
+  service_endpoint_name = "azure-devops-github-rw"
+}
+
+data "azuredevops_serviceendpoint_github" "azure_devops_github_ro" {
+  project_id            = local.devops_project_id
+  service_endpoint_name = "azure-devops-github-ro"
 }
