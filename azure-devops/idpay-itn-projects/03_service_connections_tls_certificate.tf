27dac3f9b88527046393bfe0d2a6f512fcc7f31b
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-04-18 19:29:43 +0200

    refactor TLS certificate modules for environment consistency

--- azure-devops/idpay-itn-projects/03_service_connections_tls_certificate.tf
+++ azure-devops/idpay-itn-projects/03_service_connections_tls_certificate.tf
@@ -1,138 +1,74 @@
 #
 # 🟢 DEV - TLS CERTIFICATE
 #
-module "letsencrypt_dev" {
-  source = "./.terraform/modules/__v3__/letsencrypt_credential"
+module "dev_tls_cert_service_connection" {
+  source = "../_module_collection/tls_cert_service_connection"
 
   providers = {
     azurerm = azurerm.dev
   }
 
-  prefix            = local.prefix
-  env               = "d"
-  key_vault_name    = local.dev_kv_domain_name
-  subscription_name = data.azurerm_subscriptions.dev.subscriptions[0].display_name
-}
+  env_short       = "d"
+  prefix          = local.prefix
+  location        = local.location
+  azdo_project_id = data.azuredevops_project.this.id
 
-module "dev_tls_cert_service_conn" {
-  source = "./.terraform/modules/__devops_v0__/azuredevops_serviceendpoint_federated"
+  identity_name                = "${local.product}-dev-${local.domain}-azdo-tls-cert"
+  identity_resource_group_name = local.dev_identity_rg_name
+  key_vault_id                 = data.azurerm_key_vault.dev_kv_domain.id
+  key_vault_name               = local.dev_kv_domain_name
 
-  providers = {
-    azurerm = azurerm.dev
-  }
-
-  project_id = data.azuredevops_project.this.id
-  #tfsec:ignore:general-secrets-no-plaintext-exposure
-  name              = "${local.project}-dev-azdo-tls-cert"
   tenant_id         = data.azurerm_client_config.current.tenant_id
-  subscription_id   = local.dev_subscription_id
   subscription_name = local.dev_subscription_name
-
-  location            = local.location
-  resource_group_name = local.dev_identity_rg_name
-
+  subscription_id   = local.dev_subscription_id
 }
 
-resource "azurerm_key_vault_access_policy" "dev_tls_cert_service_conn" {
-  provider = azurerm.dev
+#
+# 🟨 UAT - TLS CERTIFICATE
+#
+module "uat_tls_cert_service_connection" {
+  source = "../_module_collection/tls_cert_service_connection"
 
-  key_vault_id = data.azurerm_key_vault.dev_kv_domain.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = module.dev_tls_cert_service_conn.service_principal_object_id
+  providers = {
+    azurerm = azurerm.uat
+  }
 
-  certificate_permissions = ["Get", "Import"]
+  env_short       = "u"
+  prefix          = local.prefix
+  location        = local.location
+  azdo_project_id = data.azuredevops_project.this.id
+
+  identity_name                = "${local.product}-uat-${local.domain}-azdo-tls-cert"
+  identity_resource_group_name = local.uat_identity_rg_name
+  key_vault_id                 = data.azurerm_key_vault.uat_kv_domain.id
+  key_vault_name               = local.uat_kv_domain_name
+
+  tenant_id         = data.azurerm_client_config.current.tenant_id
+  subscription_name = local.uat_subscription_name
+  subscription_id   = local.uat_subscription_id
 }
 
-# #
-# # 🟨 UAT - TLS CERTIFICATE
-# #
-# module "letsencrypt_uat" {
-#   source = "./.terraform/modules/__v3__/letsencrypt_credential"
 #
-#   providers = {
-#     azurerm = azurerm.uat
-#   }
+# 🛑 PROD - TLS CERTIFICATE
 #
-#   prefix            = local.prefix
-#   env               = "u"
-#   key_vault_name    = local.uat_kv_domain_name
-#   subscription_name = data.azurerm_subscriptions.uat.subscriptions[0].display_name
-# }
-#
-# module "uat_tls_cert_service_conn" {
-#   source = "./.terraform/modules/__devops_v0__/azuredevops_serviceendpoint_federated"
-#
-#   providers = {
-#     azurerm = azurerm.uat
-#   }
-#
-#   project_id = data.azuredevops_project.this.id
-#   #tfsec:ignore:general-secrets-no-plaintext-exposure
-#   name              = "${local.project}-uat-azdo-tls-cert"
-#   tenant_id         = data.azurerm_client_config.current.tenant_id
-#   subscription_id   = data.azurerm_subscriptions.uat.subscriptions[0].subscription_id
-#   subscription_name = data.azurerm_subscriptions.uat.subscriptions[0].display_name
-#
-#   location            = local.location
-#   resource_group_name = local.uat_identity_rg_name
-#
-# }
-#
-#
-# resource "azurerm_key_vault_access_policy" "uat_tls_cert_service_conn" {
-#   provider = azurerm.uat
-#
-#   key_vault_id = data.azurerm_key_vault.uat_kv_domain.id
-#   tenant_id    = data.azurerm_client_config.current.tenant_id
-#   object_id    = module.uat_tls_cert_service_conn.service_principal_object_id
-#
-#   certificate_permissions = ["Get", "Import"]
-# }
-#
-#
-# #
-# # 🛑 PROD - TLS CERTIFICATE
-# #
-# module "letsencrypt_prod" {
-#   source = "./.terraform/modules/__v3__/letsencrypt_credential"
-#
-#   providers = {
-#     azurerm = azurerm.prod
-#   }
-#
-#   prefix            = local.prefix
-#   env               = "p"
-#   key_vault_name    = local.prod_kv_domain_name
-#   subscription_name = data.azurerm_subscriptions.prod.subscriptions[0].display_name
-# }
-#
-# module "prod_tls_cert_service_conn" {
-#   source = "./.terraform/modules/__devops_v0__/azuredevops_serviceendpoint_federated"
-#
-#   providers = {
-#     azurerm = azurerm.prod
-#   }
-#
-#   project_id = data.azuredevops_project.this.id
-#   #tfsec:ignore:general-secrets-no-plaintext-exposure
-#   name              = "${local.project}-prod-azdo-tls-cert"
-#   tenant_id         = data.azurerm_client_config.current.tenant_id
-#   subscription_id   = data.azurerm_subscriptions.prod.subscriptions[0].subscription_id
-#   subscription_name = data.azurerm_subscriptions.prod.subscriptions[0].display_name
-#
-#   location            = local.location
-#   resource_group_name = local.prod_identity_rg_name
-#
-#   depends_on = [module.letsencrypt_prod]
-# }
-#
-#
-# resource "azurerm_key_vault_access_policy" "prod_tls_cert_service_conn" {
-#   provider = azurerm.prod
-#
-#   key_vault_id = data.azurerm_key_vault.prod_kv_domain.id
-#   tenant_id    = data.azurerm_client_config.current.tenant_id
-#   object_id    = module.prod_tls_cert_service_conn.service_principal_object_id
-#
-#   certificate_permissions = ["Get", "Import"]
-# }
+module "prod_tls_cert_service_connection" {
+  source = "../_module_collection/tls_cert_service_connection"
+
+  providers = {
+    azurerm = azurerm.prod
+  }
+
+  env_short       = "p"
+  prefix          = local.prefix
+  location        = local.location
+  azdo_project_id = data.azuredevops_project.this.id
+
+  identity_name                = "${local.product}-prod-${local.domain}-azdo-tls-cert"
+  identity_resource_group_name = local.prod_identity_rg_name
+  key_vault_id                 = data.azurerm_key_vault.prod_kv_domain.id
+  key_vault_name               = local.prod_kv_domain_name
+
+  tenant_id         = data.azurerm_client_config.current.tenant_id
+  subscription_name = local.prod_subscription_name
+  subscription_id   = local.prod_subscription_id
+}
--- azure-devops/idpay-itn-projects/99_locals.tf
+++ azure-devops/idpay-itn-projects/99_locals.tf
@@ -1,26 +1,26 @@
 locals {
-  prefix         = "cstar"
-  domain         = "idpay"
-  location_short = "itn"
-  location       = "italynorth"
-  azdo_project_name  = "cstar-platform-app-projects"
-  project = "${local.prefix}-${local.location_short}-${local.domain}"
+  prefix            = "cstar"
+  domain            = "idpay"
+  location_short    = "itn"
+  location          = "italynorth"
+  azdo_project_name = "cstar-platform-app-projects"
+  product           = "${local.prefix}-${local.location_short}"
 
   # Subscriptions
-  dev_subscription_name        = data.azurerm_subscriptions.dev.subscriptions[0].display_name
-  uat_subscription_name        = data.azurerm_subscriptions.uat.subscriptions[0].display_name
-  prod_subscription_name        = data.azurerm_subscriptions.prod.subscriptions[0].display_name
+  dev_subscription_name  = data.azurerm_subscriptions.dev.subscriptions[0].display_name
+  uat_subscription_name  = data.azurerm_subscriptions.uat.subscriptions[0].display_name
+  prod_subscription_name = data.azurerm_subscriptions.prod.subscriptions[0].display_name
 
-  dev_subscription_id        = data.azurerm_subscriptions.dev.subscriptions[0].subscription_id
-  uat_subscription_id        = data.azurerm_subscriptions.uat.subscriptions[0].subscription_id
-  prod_subscription_id        = data.azurerm_subscriptions.prod.subscriptions[0].subscription_id
+  dev_subscription_id  = data.azurerm_subscriptions.dev.subscriptions[0].subscription_id
+  uat_subscription_id  = data.azurerm_subscriptions.uat.subscriptions[0].subscription_id
+  prod_subscription_id = data.azurerm_subscriptions.prod.subscriptions[0].subscription_id
 
 
   # 🔑 Key Vault
-  dev_kv_domain_name           = "${local.prefix}-d-${local.location_short}-${local.domain}-kv"
-  dev_kv_domain_resource_group = "${local.prefix}-d-${local.location_short}-${local.domain}-security-rg"
-  uat_kv_domain_name           = "${local.prefix}-u-${local.location_short}-${local.domain}-kv"
-  uat_kv_domain_resource_group = "${local.prefix}-u-${local.location_short}-${local.domain}-security-rg"
+  dev_kv_domain_name            = "${local.prefix}-d-${local.location_short}-${local.domain}-kv"
+  dev_kv_domain_resource_group  = "${local.prefix}-d-${local.location_short}-${local.domain}-security-rg"
+  uat_kv_domain_name            = "${local.prefix}-u-${local.location_short}-${local.domain}-kv"
+  uat_kv_domain_resource_group  = "${local.prefix}-u-${local.location_short}-${local.domain}-security-rg"
   prod_kv_domain_name           = "${local.prefix}-p-${local.location_short}-${local.domain}-kv"
   prod_kv_domain_resource_group = "${local.prefix}-p-${local.location_short}-${local.domain}-security-rg"
 
@@ -34,104 +34,10 @@ locals {
   prod_dns_zone_name = "cstar.pagopa.it"
 
   # Idenity RG
-  dev_identity_rg_name = "cstar-d-itn-idpay-cicd-rg"
-  uat_identity_rg_name = "cstar-u-itn-idpay-cicd-rg"
+  dev_identity_rg_name  = "cstar-d-itn-idpay-cicd-rg"
+  uat_identity_rg_name  = "cstar-u-itn-idpay-cicd-rg"
   prod_identity_rg_name = "cstar-p-itn-idpay-cicd-rg"
 
-
-  # product         = "${local.prefix}-${local.domain}"
-  # external_domain = "pagopa.it"
-  # dns_zone_third  = "idpay.pagopa.it"
-
-  # # ⚙️ DevOps
-  # devops_project_id      = data.azuredevops_project.this.project_id
-  # domain_namespace       = local.domain
-  # srv_endpoint_sonar     = data.azuredevops_serviceendpoint_sonarcloud.azure_devops_sonar.id
-  # srv_endpoint_github_id = data.azuredevops_serviceendpoint_github.arc_azure_devops_github_rw.id
-
-  # # 🟢 DEV
-  # dev_subscription_name        = "dev-${local.prefix}"
-  # dev_kv_domain_name           = "${local.prefix}-d-${local.location_short}-${local.domain}-kv"
-  # dev_kv_domain_resource_group = "${local.prefix}-d-${local.location_short}-idpay-sec-rg"
-  # # dev_aks_name                 = "${local.prefix}-d-${local.location_short}-dev-aks"
-  # # dev_azurecr_name             = "${local.prefix}d${local.location_short}corecommonacr"
-  # dev_azdo_agent_pool = "${local.prefix}-dev-linux-app"
-
-  # dev_apim_prefix_domain     = "https://api.dev.idpay.pagopa.it"
-  # dev_storage_account_rg     = "${local.prefix}-d-${local.location_short}-${local.domain}-cdn-rg"
-  # dev_storage_account_name   = replace("${local.prefix}-d-${local.domain}-sa", "-", "")
-  # dev_cdn_profile            = "${local.prefix}-d-${local.domain}-cdn-profile"
-  # dev_cdn_endpoint           = "${local.prefix}-d-${local.domain}-cdn-endpoint"
-  # dev_checkout_prefix_domain = "https://dev.checkout.pagopa.it"
-  # dev_checkout_platform_url  = "https://api.dev.platform.pagopa.it/checkout/ec/v1"
-  # dev_payment_return_url     = "https://dev.idpay.pagopa.it/pagamenti"
-
-  # # dev_srv_endpoint_aks_name                 = "${local.prefix}-${local.domain}-aks-dev"
-  # # dev_srv_endpoint_azure_devops_docker_name = data.azuredevops_serviceendpoint_azurecr.dev_ita_workload_identity.service_endpoint_name
-  # # dev_srv_endpoint_azure_devops_docker_id   = data.azuredevops_serviceendpoint_azurecr.dev_ita_workload_identity.id
-  # # dev_srv_endpoint_aks_id                   = azuredevops_serviceendpoint_kubernetes.aks_dev.id
-  # dev_srv_endpoint_azure_id   = data.azuredevops_serviceendpoint_azurerm.dev_azurerm_service_conn.id
-  # dev_srv_endpoint_azure_name = data.azuredevops_serviceendpoint_azurerm.dev_azurerm_service_conn.service_endpoint_name
-  # dev_srv_endpoint_tls_id     = module.dev_tls_cert_service_conn.service_endpoint_id
-
-  # dev_identity_rg_name = "${local.prefix}-d-${local.location_short}-core-identity-rg"
-
-  # # 🟨 UAT
-  # uat_subscription_name        = "uat-${local.prefix}"
-  # uat_kv_domain_name           = "${local.prefix}-u-${local.location_short}-${local.domain}-kv"
-  # uat_kv_domain_resource_group = "${local.prefix}-u-${local.location_short}-idpay-sec-rg"
-  # # uat_aks_name                 = "${local.prefix}-u-${local.location_short}-uat-aks"
-  # # uat_azurecr_name             = "${local.prefix}u${local.location_short}corecommonacr"
-  # uat_azdo_agent_pool = "${local.prefix}-uat-linux-app"
-
-  # uat_apim_prefix_domain     = "https://api.uat.idpay.pagopa.it"
-  # uat_storage_account_rg     = "${local.prefix}-u-${local.location_short}-${local.domain}-cdn-rg"
-  # uat_storage_account_name   = replace("${local.prefix}-u-${local.domain}-sa", "-", "")
-  # uat_cdn_profile            = "${local.prefix}-u-${local.domain}-cdn-profile"
-  # uat_cdn_endpoint           = "${local.prefix}-u-${local.domain}-cdn-endpoint"
-  # uat_checkout_prefix_domain = "https://uat.checkout.pagopa.it"
-  # uat_checkout_platform_url  = "https://api.uat.platform.pagopa.it/checkout/ec/v1"
-  # uat_payment_return_url     = "https://uat.idpay.pagopa.it/pagamenti"
-
-  # # uat_srv_endpoint_aks_name                 = "${local.prefix}-${local.domain}-aks-uat"
-  # # uat_srv_endpoint_azure_devops_docker_name = data.azuredevops_serviceendpoint_azurecr.uat_ita_workload_identity.service_endpoint_name
-  # # uat_srv_endpoint_azure_devops_docker_id   = data.azuredevops_serviceendpoint_azurecr.uat_ita_workload_identity.id
-  # # uat_srv_endpoint_aks_id                   = azuredevops_serviceendpoint_kubernetes.aks_uat.id
-  # uat_srv_endpoint_azure_id   = data.azuredevops_serviceendpoint_azurerm.uat_azurerm_service_conn.id
-  # uat_srv_endpoint_azure_name = data.azuredevops_serviceendpoint_azurerm.uat_azurerm_service_conn.service_endpoint_name
-  # uat_srv_endpoint_tls_id     = module.uat_tls_cert_service_conn.service_endpoint_id
-
-  # uat_identity_rg_name = "${local.prefix}-u-${local.location_short}-core-identity-rg"
-
-  # # 🛑 PROD
-  # prod_subscription_name        = "prod-${local.prefix}"
-  # prod_kv_domain_name           = "${local.prefix}-p-${local.location_short}-${local.domain}-kv"
-  # prod_kv_domain_resource_group = "${local.prefix}-p-${local.location_short}-idpay-sec-rg"
-  # # prod_aks_name                 = "${local.prefix}-p-${local.location_short}-prod-aks"
-  # # prod_azurecr_name             = "${local.prefix}p${local.location_short}corecommonacr"
-  # prod_azdo_agent_pool = "${local.prefix}-prod-linux-app"
-
-  # prod_apim_prefix_domain     = "https://api.idpay.pagopa.it"
-  # prod_storage_account_rg     = "${local.prefix}-p-${local.location_short}-${local.domain}-cdn-rg"
-  # prod_storage_account_name   = replace("${local.prefix}-p-${local.domain}-sa", "-", "")
-  # prod_cdn_profile            = "${local.prefix}-p-${local.domain}-cdn-profile"
-  # prod_cdn_endpoint           = "${local.prefix}-p-${local.domain}-cdn-endpoint"
-  # prod_checkout_prefix_domain = "https://checkout.pagopa.it"
-  # prod_checkout_platform_url  = "https://api.platform.pagopa.it/checkout/ec/v1"
-  # prod_payment_return_url     = "https://idpay.pagopa.it/pagamenti"
-
-  # # prod_srv_endpoint_aks_name                 = "${local.prefix}-${local.domain}-aks-prod"
-  # # prod_srv_endpoint_azure_devops_docker_name = data.azuredevops_serviceendpoint_azurecr.prod_ita_workload_identity.service_endpoint_name
-  # # prod_srv_endpoint_azure_devops_docker_id   = data.azuredevops_serviceendpoint_azurecr.prod_ita_workload_identity.id
-  # # prod_srv_endpoint_aks_id                   = azuredevops_serviceendpoint_kubernetes.aks_prod.id
-  # prod_srv_endpoint_azure_id   = data.azuredevops_serviceendpoint_azurerm.prod_azurerm_service_conn.id
-  # prod_srv_endpoint_azure_name = data.azuredevops_serviceendpoint_azurerm.prod_azurerm_service_conn.service_endpoint_name
-  # prod_srv_endpoint_tls_id     = module.prod_tls_cert_service_conn.service_endpoint_id
-
-  # prod_identity_rg_name = "${local.prefix}-p-${local.location_short}-core-identity-rg"
-
-  # prod_core_kv_name           = "${local.prefix}-p-${local.location_short}-core-kv"
-  # prod_core_kv_resource_group = "${local.prefix}-p-${local.location_short}-core-sec-rg"
 }
 
 # LOCAL TLS CERT
