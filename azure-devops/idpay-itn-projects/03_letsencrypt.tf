306b4505c1e8d81f1dca0cad199b45622280fd57
Diego Mauricio Lagos <diego.lagosmorales@pagopa.it>
2025-04-18 10:58:37 +0200

    refactor: update letsencrypt module configurations for dev, uat, and prod environments

--- azure-devops/idpay-itn-projects/03_letsencrypt.tf
+++ azure-devops/idpay-itn-projects/03_letsencrypt.tf
@@ -1,48 +1,47 @@
-# #
-# # 🟢 DEV - LETSENCRYPT
-# #
-# module "letsencrypt_dev" {
-#   source = "./.terraform/modules/__v3__/letsencrypt_credential"
+#
+# 🟢 DEV - LETSENCRYPT
+#
+module "letsencrypt_dev" {
+  source = "./.terraform/modules/__v3__/letsencrypt_credential"
 
-#   providers = {
-#     azurerm = azurerm.dev
-#   }
+  providers = {
+    azurerm = azurerm.dev
+  }
 
-#   prefix            = local.prefix
-#   env               = "d"
-#   key_vault_name    = local.dev_domain_kv_name
-#   subscription_name = data.azurerm_subscriptions.dev.subscriptions[0].display_name
-# }
+  prefix            = local.prefix
+  env               = "d"
+  key_vault_name    = local.dev_kv_domain_name
+  subscription_name = data.azurerm_subscriptions.dev.subscriptions[0].display_name
+}
 
-# #
-# # 🟨 UAT - LETSENCRYPT
-# #
+#
+# 🟨 UAT - LETSENCRYPT
+#
+module "letsencrypt_uat" {
+  source = "./.terraform/modules/__v3__/letsencrypt_credential"
 
-# module "letsencrypt_uat" {
-#   source = "./.terraform/modules/__v3__/letsencrypt_credential"
+  providers = {
+    azurerm = azurerm.uat
+  }
 
-#   providers = {
-#     azurerm = azurerm.uat
-#   }
+  prefix            = local.prefix
+  env               = "u"
+  key_vault_name    = local.uat_kv_domain_name
+  subscription_name = data.azurerm_subscriptions.uat.subscriptions[0].display_name
+}
 
-#   prefix            = local.prefix
-#   env               = "u"
-#   key_vault_name    = local.uat_domain_kv_name
-#   subscription_name = data.azurerm_subscriptions.uat.subscriptions[0].display_name
-# }
+#
+# 🛑 PROD - LETSENCRYPT
+#
+module "letsencrypt_prod" {
+  source = "./.terraform/modules/__v3__/letsencrypt_credential"
 
-# #
-# # 🛑 PROD - LETSENCRYPT
-# #
-# module "letsencrypt_prod" {
-#   source = "./.terraform/modules/__v3__/letsencrypt_credential"
+  providers = {
+    azurerm = azurerm.prod
+  }
 
-#   providers = {
-#     azurerm = azurerm.prod
-#   }
-
-#   prefix            = local.prefix
-#   env               = "p"
-#   key_vault_name    = local.prod_domain_kv_name
-#   subscription_name = data.azurerm_subscriptions.prod.subscriptions[0].display_name
-# }
+  prefix            = local.prefix
+  env               = "p"
+  key_vault_name    = local.prod_kv_domain_name
+  subscription_name = data.azurerm_subscriptions.prod.subscriptions[0].display_name
+}
