ab84ef930645ba663b11c3d47d5f89386f189800
stefanodel <stefano11.delia@nttdata.com>
2025-04-24 15:06:55 +0200

    feat: add fe pipeline for idpay portals

--- azure-devops/idpay-itn-projects/00_data.tf
+++ azure-devops/idpay-itn-projects/00_data.tf
@@ -18,3 +18,18 @@ data "azurerm_subscriptions" "uat" {
 data "azurerm_subscriptions" "dev" {
   display_name_prefix = local.dev_subscription_name
 }
+
+data "azuredevops_serviceendpoint_azurerm" "dev_azurerm_service_conn" {
+  project_id            = local.devops_project_id
+  service_endpoint_name = "${data.azurerm_subscriptions.dev.subscriptions[0].display_name}-AZURE-SERVICE-CONN"
+}
+
+data "azuredevops_serviceendpoint_azurerm" "uat_azurerm_service_conn" {
+  project_id            = local.devops_project_id
+  service_endpoint_name = "${data.azurerm_subscriptions.uat.subscriptions[0].display_name}-AZURE-SERVICE-CONN"
+}
+
+data "azuredevops_serviceendpoint_azurerm" "prod_azurerm_service_conn" {
+  project_id            = local.devops_project_id
+  service_endpoint_name = "${data.azurerm_subscriptions.prod.subscriptions[0].display_name}-AZURE-SERVICE-CONN"
+}
