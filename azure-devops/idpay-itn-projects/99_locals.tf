c0892ea734442d780061a4916783fd4abaa86e5f
Andrea Morabito <78792023+and-mora@users.noreply.github.com>
2025-06-30 18:07:26 +0200

    feat: add pipeline definition (#149)
    * add pipeline definition
    
    * Refactoring env vars + add keycloak env
    
    * Pre-commit
    
    * Update var name
    
    * change variable name
    
    * fix var name
    
    * Pre-commit
    
    * remove unused var
    
    * change keycloak dev url
    
    ---------
    
    Co-authored-by: angelosca24 <angelo.scagliola@nttdata.com>
    Co-authored-by: Christian-Gen00 <christian.genovese@emeal.nttdata.com>
    Co-authored-by: Christian Geovese <cgenoves@emeal.nttdata.com>

--- azure-devops/idpay-itn-projects/99_locals.tf
+++ azure-devops/idpay-itn-projects/99_locals.tf
@@ -201,6 +201,9 @@ locals {
   dev_react_app_asset_register_url_fe_login             = "https://dev.selfcare.pagopa.it/auth"
   dev_react_app_asset_register_url_fe_assistance_portal = "/registro-dei-beni/assistenza"
   dev_react_app_asset_register_url_fe_landing           = "https://dev.selfcare.pagopa.it/auth/logout"
+  dev_vite_keycloak_url                                 = "https://api-mcshared.dev.cstar.pagopa.it/auth-itn"
+  dev_vite_keycloak_realm                               = "merchant-operator"
+  dev_vite_keycloak_client_id                           = "frontend"
 
   uat_react_app_asset_register_url_cdn                  = "https://registrodeibeni.uat.cstar.pagopa.it/"
   uat_react_app_asset_register_url_storage              = "https://cstaruitnidpayregcdnsa.z38.web.core.windows.net"
@@ -208,6 +211,9 @@ locals {
   uat_react_app_asset_register_url_fe_login             = "https://uat.selfcare.pagopa.it/auth"
   uat_react_app_asset_register_url_fe_assistance_portal = "/registro-dei-beni/assistenza"
   uat_react_app_asset_register_url_fe_landing           = "https://uat.selfcare.pagopa.it/auth/logout"
+  uat_vite_keycloak_url                                 = "TODO"
+  uat_vite_keycloak_realm                               = "merchant-operator"
+  uat_vite_keycloak_client_id                           = "frontend"
 
   prod_react_app_asset_register_url_cdn                  = "https://registrodeibeni.cstar.pagopa.it/"
   prod_react_app_asset_register_url_storage              = "https://cstarpitnidpayregcdnsa.z38.web.core.windows.net"
@@ -215,7 +221,9 @@ locals {
   prod_react_app_asset_register_url_fe_login             = "https://selfcare.pagopa.it/auth"
   prod_react_app_asset_register_url_fe_assistance_portal = "/registro-dei-beni/assistenza"
   prod_react_app_asset_register_url_fe_landing           = "https://selfcare.pagopa.it/auth/logout"
-
+  prod_vite_keycloak_url                                 = "TODO"
+  prod_vite_keycloak_realm                               = "merchant-operator"
+  prod_vite_keycloak_client_id                           = "frontend"
 }
 
 # LOCAL TLS CERT
