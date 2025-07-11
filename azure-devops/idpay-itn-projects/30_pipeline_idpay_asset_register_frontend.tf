772e69edbeffaf32dc9ae216d2616848235752da
Christian Geovese <cgenoves@emeal.nttdata.com>
2025-07-11 17:46:57 +0200

    add and fix env var

--- azure-devops/idpay-itn-projects/30_pipeline_idpay_asset_register_frontend.tf
+++ azure-devops/idpay-itn-projects/30_pipeline_idpay_asset_register_frontend.tf
@@ -24,7 +24,7 @@ locals {
     DEV_AZURE_SUBSCRIPTION                 = local.dev_service_endpoint_azure_id
     DEV_CDN_ENDPOINT                       = local.dev_cdn_endpoint
     DEV_CDN_PROFILE                        = local.dev_cdn_profile
-    DEV_RESOURCE_GROUP                     = local.dev_storage_account_rg
+    DEV_RESOURCE_GROUP                     = local.dev_welfare_storage_account_rg
     DEV_STORAGE_ACCOUNT                    = local.dev_storage_asset_register_account_name
     DEV_REACT_APP_URL_CDN                  = local.dev_react_app_asset_register_url_cdn
     DEV_REACT_APP_URL_FE_PRE_LOGIN         = local.dev_react_app_asset_register_url_fe_pre_login
@@ -36,7 +36,7 @@ locals {
     UAT_AZURE_SUBSCRIPTION                 = local.uat_service_endpoint_azure_id
     UAT_CDN_ENDPOINT                       = local.uat_cdn_endpoint
     UAT_CDN_PROFILE                        = local.uat_cdn_profile
-    UAT_RESOURCE_GROUP                     = local.uat_storage_account_rg
+    UAT_RESOURCE_GROUP                     = local.uat_welfare_storage_account_rg
     UAT_STORAGE_ACCOUNT                    = local.uat_storage_asset_register_account_name
     UAT_REACT_APP_URL_CDN                  = local.uat_react_app_asset_register_url_cdn
     UAT_REACT_APP_URL_FE_PRE_LOGIN         = local.uat_react_app_asset_register_url_fe_pre_login
@@ -48,7 +48,7 @@ locals {
     PROD_AZURE_SUBSCRIPTION                 = local.prod_service_endpoint_azure_id
     PROD_CDN_ENDPOINT                       = local.prod_cdn_endpoint
     PROD_CDN_PROFILE                        = local.prod_cdn_profile
-    PROD_RESOURCE_GROUP                     = local.prod_storage_account_rg
+    PROD_RESOURCE_GROUP                     = local.prod_welfare_storage_account_rg
     PROD_STORAGE_ACCOUNT                    = local.prod_storage_asset_register_account_name
     PROD_REACT_APP_URL_CDN                  = local.prod_react_app_asset_register_url_cdn
     PROD_REACT_APP_URL_FE_PRE_LOGIN         = local.prod_react_app_asset_register_url_fe_pre_login
