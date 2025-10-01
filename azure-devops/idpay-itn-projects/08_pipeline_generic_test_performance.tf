1f17e443356e5b8b6161e0a65468092de072c71d
diegoitaliait <diego.lagosmorales@pagopa.it>
2025-10-01 16:40:47 +0200

    feat: IDP-2801 idpay k6 generic test performance pipeline (#184)
    * added new pipeline generic performance test
    
    * fix: update pipeline YAML filename for generic performance test
    
    * fix folder

---
+++ azure-devops/idpay-itn-projects/08_pipeline_generic_test_performance.tf
@@ -0,0 +1,54 @@
+variable "pari_generic_performance_test" {
+  default = {
+    repository = {
+      organization    = "pagopa"
+      name            = "pari-performance-test"
+      branch_name     = "refs/heads/main"
+      pipelines_path  = ".devops"
+      yml_prefix_name = null
+    }
+    pipeline = {
+      path = "idpay\\pari-performance-test"
+    }
+  }
+}
+
+locals {
+  # global vars
+  pari_generic_performance_test_variables = {}
+
+  # global secrets
+  pari_generic_performance_test_variables_secret = {}
+
+}
+
+module "pari_generic_performance_test" {
+  source = "./.terraform/modules/__devops_v0__/azuredevops_build_definition_generic"
+
+  providers = {
+    azurerm = azurerm.prod
+  }
+
+  project_id                   = local.devops_project_id
+  repository                   = var.pari_generic_performance_test.repository
+  github_service_connection_id = local.service_connection_github_ro_id
+
+  pipeline_name         = "k6-performance-generic.test"
+  pipeline_yml_filename = "k6-performance-generic.yml"
+  path                  = var.pari_generic_performance_test.pipeline.path
+
+  variables = merge(
+    local.pari_generic_performance_test_variables,
+  )
+
+  variables_secret = merge(
+    local.pari_generic_performance_test_variables_secret
+  )
+
+  service_connection_ids_authorization = [
+    local.service_connection_github_ro_id,
+    local.dev_service_endpoint_azure_id,
+    local.uat_service_endpoint_azure_id,
+    local.prod_service_endpoint_azure_id
+  ]
+}
