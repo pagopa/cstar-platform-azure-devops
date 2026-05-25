e9776ef78e7b865a976f2f8ac196edefb5e8e5a8
marco.meloni <marco.meloni@emeal.nttdata.com>
2026-05-25 17:38:02 +0200

    chore: add mail assistance from secrets

--- azure-devops/mdc-itn-projects/30_pipeline_emd_ar_backoffice_fe.tf
+++ azure-devops/mdc-itn-projects/30_pipeline_emd_ar_backoffice_fe.tf
@@ -18,57 +18,59 @@ locals {
   dev_backoffice_resource_group  = "${local.prefix}-d-${local.location_short}-${local.domain}-data-rg"
   dev_backoffice_storage_account = replace("${local.prefix}-d-${local.location_short}-${local.domain}backoffice", "-", "")
   dev_backoffice_cdn_domain      = "mdc.dev.cstar.pagopa.it"
-  # TODO: sostituire con URL APIM quando configurato (es. https://api.dev.cstar.pagopa.it/emd/ar/backoffice)
-  # Per ora punta all'ingress interno AKS — NON raggiungibile dal browser, solo placeholder
-  dev_backoffice_api_base_url = "https://api-emd.dev.cstar.pagopa.it/emd/ar/backoffice"
-  dev_backoffice_ar_base_url  = "https://dev.selfcare.pagopa.it/auth"
+  dev_backoffice_api_base_url    = "https://api-emd.dev.cstar.pagopa.it/emd/backoffice"
+  dev_backoffice_ar_base_url     = "https://dev.selfcare.pagopa.it"
+  dev_assistance_email           = module.domain_dev_secrets.values["email-mdc-google"].value
 
   uat_backoffice_cdn_profile     = "${local.prefix}-u-${local.location_short}-${local.domain}-backoffice"
   uat_backoffice_cdn_endpoint    = "${local.prefix}-u-${local.location_short}-${local.domain}-backoffice"
   uat_backoffice_resource_group  = "${local.prefix}-u-${local.location_short}-${local.domain}-data-rg"
   uat_backoffice_storage_account = replace("${local.prefix}-u-${local.location_short}-${local.domain}backoffice", "-", "")
   uat_backoffice_cdn_domain      = "mdc.uat.cstar.pagopa.it"
-  # TODO: sostituire con URL APIM quando configurato
-  uat_backoffice_api_base_url = "https://api-emd.uat.cstar.pagopa.it/emd/ar/backoffice"
-  uat_backoffice_ar_base_url  = "https://uat.selfcare.pagopa.it/auth"
+  uat_backoffice_api_base_url    = "https://api-emd.uat.cstar.pagopa.it/emd/backoffice"
+  uat_backoffice_ar_base_url     = "https://uat.selfcare.pagopa.it"
+  uat_assistance_email           = module.domain_uat_secrets.values["email-mdc-google"].value
 
   prod_backoffice_cdn_profile     = "${local.prefix}-p-${local.location_short}-${local.domain}-backoffice"
   prod_backoffice_cdn_endpoint    = "${local.prefix}-p-${local.location_short}-${local.domain}-backoffice"
   prod_backoffice_resource_group  = "${local.prefix}-p-${local.location_short}-${local.domain}-data-rg"
   prod_backoffice_storage_account = replace("${local.prefix}-p-${local.location_short}-${local.domain}backoffice", "-", "")
   prod_backoffice_cdn_domain      = "mdc.cstar.pagopa.it"
-  # TODO: sostituire con URL APIM quando configurato
-  prod_backoffice_api_base_url = "https://api-emd.cstar.pagopa.it/emd/ar/backoffice"
-  prod_backoffice_ar_base_url  = "https://selfcare.pagopa.it/auth"
+  prod_backoffice_api_base_url    = "https://api-emd.cstar.pagopa.it/emd/backoffice"
+  prod_backoffice_ar_base_url     = "https://selfcare.pagopa.it"
+  prod_assistance_email           = module.domain_prod_secrets.values["email-mdc-google"].value
 
   emd_ar_backoffice_fe_variables_deploy = {
     # AzureCLI@2 richiede il NOME della service connection (non l'id)
-    DEV_AZURE_SUBSCRIPTION = local.dev_service_endpoint_azure_name
-    DEV_CDN_ENDPOINT       = local.dev_backoffice_cdn_endpoint
-    DEV_CDN_PROFILE        = local.dev_backoffice_cdn_profile
-    DEV_RESOURCE_GROUP     = local.dev_backoffice_resource_group
-    DEV_STORAGE_ACCOUNT    = local.dev_backoffice_storage_account
-    DEV_CDN_DOMAINS        = local.dev_backoffice_cdn_domain
-    DEV_API_BASE_URL       = local.dev_backoffice_api_base_url
-    DEV_AR_BASE_URL        = local.dev_backoffice_ar_base_url
+    DEV_AZURE_SUBSCRIPTION  = local.dev_service_endpoint_azure_name
+    DEV_CDN_ENDPOINT        = local.dev_backoffice_cdn_endpoint
+    DEV_CDN_PROFILE         = local.dev_backoffice_cdn_profile
+    DEV_RESOURCE_GROUP      = local.dev_backoffice_resource_group
+    DEV_STORAGE_ACCOUNT     = local.dev_backoffice_storage_account
+    DEV_CDN_DOMAINS         = local.dev_backoffice_cdn_domain
+    DEV_API_BASE_URL        = local.dev_backoffice_api_base_url
+    DEV_AR_BASE_URL         = local.dev_backoffice_ar_base_url
+    DEV_MDC_ASSISTANCE_MAIL = local.dev_assistance_email
 
-    UAT_AZURE_SUBSCRIPTION = local.uat_service_endpoint_azure_name
-    UAT_CDN_ENDPOINT       = local.uat_backoffice_cdn_endpoint
-    UAT_CDN_PROFILE        = local.uat_backoffice_cdn_profile
-    UAT_RESOURCE_GROUP     = local.uat_backoffice_resource_group
-    UAT_STORAGE_ACCOUNT    = local.uat_backoffice_storage_account
-    UAT_CDN_DOMAINS        = local.uat_backoffice_cdn_domain
-    UAT_API_BASE_URL       = local.uat_backoffice_api_base_url
-    UAT_AR_BASE_URL        = local.uat_backoffice_ar_base_url
+    UAT_AZURE_SUBSCRIPTION  = local.uat_service_endpoint_azure_name
+    UAT_CDN_ENDPOINT        = local.uat_backoffice_cdn_endpoint
+    UAT_CDN_PROFILE         = local.uat_backoffice_cdn_profile
+    UAT_RESOURCE_GROUP      = local.uat_backoffice_resource_group
+    UAT_STORAGE_ACCOUNT     = local.uat_backoffice_storage_account
+    UAT_CDN_DOMAINS         = local.uat_backoffice_cdn_domain
+    UAT_API_BASE_URL        = local.uat_backoffice_api_base_url
+    UAT_AR_BASE_URL         = local.uat_backoffice_ar_base_url
+    UAT_MDC_ASSISTANCE_MAIL = local.uat_assistance_email
 
-    PROD_AZURE_SUBSCRIPTION = local.prod_service_endpoint_azure_name
-    PROD_CDN_ENDPOINT       = local.prod_backoffice_cdn_endpoint
-    PROD_CDN_PROFILE        = local.prod_backoffice_cdn_profile
-    PROD_RESOURCE_GROUP     = local.prod_backoffice_resource_group
-    PROD_STORAGE_ACCOUNT    = local.prod_backoffice_storage_account
-    PROD_CDN_DOMAINS        = local.prod_backoffice_cdn_domain
-    PROD_API_BASE_URL       = local.prod_backoffice_api_base_url
-    PROD_AR_BASE_URL        = local.prod_backoffice_ar_base_url
+    PROD_AZURE_SUBSCRIPTION  = local.prod_service_endpoint_azure_name
+    PROD_CDN_ENDPOINT        = local.prod_backoffice_cdn_endpoint
+    PROD_CDN_PROFILE         = local.prod_backoffice_cdn_profile
+    PROD_RESOURCE_GROUP      = local.prod_backoffice_resource_group
+    PROD_STORAGE_ACCOUNT     = local.prod_backoffice_storage_account
+    PROD_CDN_DOMAINS         = local.prod_backoffice_cdn_domain
+    PROD_API_BASE_URL        = local.prod_backoffice_api_base_url
+    PROD_AR_BASE_URL         = local.prod_backoffice_ar_base_url
+    PROD_MDC_ASSISTANCE_MAIL = local.prod_assistance_email
   }
 }
 
