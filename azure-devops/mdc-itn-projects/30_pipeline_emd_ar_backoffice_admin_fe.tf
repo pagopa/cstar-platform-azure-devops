0af114f5218b93e5f20a2cfd916c6a8fd2f0ec87
Marco-Meloni-30 <marco.meloni@emeal.nttdata.com>
2026-07-14 09:32:03 +0200

    chore: MMC-806 add emd backoffice internal frontend pipeline (#205)
    * chore: add emd backoffice internal frontend pipeline
    
    * chore: use locals of backoffice fe and change name locals storage account

---
+++ azure-devops/mdc-itn-projects/30_pipeline_emd_ar_backoffice_admin_fe.tf
@@ -0,0 +1,69 @@
+variable "emd_ar_backoffice_admin_fe" {
+  default = {
+    repository = {
+      organization   = "pagopa"
+      name           = "emd-ar-backoffice-admin-fe"
+      branch_name    = "refs/heads/main"
+      pipelines_path = ".devops"
+    }
+    pipeline = {
+      path = "mdc\\emd-ar-backoffice-admin-fe"
+    }
+  }
+}
+
+locals {
+  dev_backoffice_admin_storage_account = replace("${local.prefix}-d-${local.location_short}-${local.domain}admin", "-", "")
+
+  uat_backoffice_admin_storage_account = replace("${local.prefix}-u-${local.location_short}-${local.domain}admin", "-", "")
+
+  prod_backoffice_admin_storage_account = replace("${local.prefix}-p-${local.location_short}-${local.domain}admin", "-", "")
+
+  emd_ar_backoffice_admin_fe_variables_deploy = {
+    # AzureCLI@2 richiede il NOME della service connection (non l'id)
+    DEV_AGENT_POOL         = local.azdo_agent_pool_dev
+    DEV_AZURE_SUBSCRIPTION = local.dev_service_endpoint_azure_name
+    DEV_STORAGE_ACCOUNT    = local.dev_backoffice_admin_storage_account
+    DEV_API_BASE_URL       = local.dev_backoffice_api_base_url
+
+    UAT_AGENT_POOL         = local.azdo_agent_pool_uat
+    UAT_AZURE_SUBSCRIPTION = local.uat_service_endpoint_azure_name
+    UAT_STORAGE_ACCOUNT    = local.uat_backoffice_admin_storage_account
+    UAT_API_BASE_URL       = local.uat_backoffice_api_base_url
+
+    PROD_AGENT_POOL         = local.azdo_agent_pool_prod
+    PROD_AZURE_SUBSCRIPTION = local.prod_service_endpoint_azure_name
+    PROD_STORAGE_ACCOUNT    = local.prod_backoffice_admin_storage_account
+    PROD_API_BASE_URL       = local.prod_backoffice_api_base_url
+  }
+}
+
+module "emd_ar_backoffice_admin_fe_deploy" {
+  source = "./.terraform/modules/__devops_v0__/azuredevops_build_definition_generic"
+
+  providers = {
+    azurerm = azurerm.prod
+  }
+
+  project_id                   = local.devops_project_id
+  repository                   = var.emd_ar_backoffice_admin_fe.repository
+  github_service_connection_id = azuredevops_serviceendpoint_github.bot_github_pr.id
+
+  pipeline_name         = "emd_ar_backoffice_admin_fe.deploy"
+  pipeline_yml_filename = "deploy-pipelines.yml"
+  path                  = var.emd_ar_backoffice_admin_fe.pipeline.path
+
+  # Nessun trigger automatico: il deploy è sempre manuale con parametri ENV e VERSION
+  ci_trigger_enabled  = false
+  ci_trigger_use_yaml = true
+
+  variables        = local.emd_ar_backoffice_admin_fe_variables_deploy
+  variables_secret = {}
+
+  service_connection_ids_authorization = [
+    azuredevops_serviceendpoint_github.bot_github_pr.id,
+    local.dev_service_endpoint_azure_id,
+    local.uat_service_endpoint_azure_id,
+    local.prod_service_endpoint_azure_id
+  ]
+}
