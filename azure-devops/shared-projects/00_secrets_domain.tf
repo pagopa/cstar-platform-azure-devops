2dcd451bff79726d524376cde83fae27fb5d08d1
umbcoppolabottazzi <umberto.coppolabottazzi@pagopa.it>
2026-07-30 13:34:46 +0200

    add shared domain

---
+++ azure-devops/shared-projects/00_secrets_domain.tf
@@ -0,0 +1,61 @@
+#
+# 🟢 DEV - SECRET
+#
+
+module "domain_dev_secrets" {
+  providers = {
+    azurerm = azurerm.dev
+  }
+
+  source = "./.terraform/modules/__v3__/key_vault_secrets_query"
+
+  key_vault_name = local.dev_kv_domain_name
+  resource_group = local.dev_kv_domain_resource_group
+
+  secrets = [
+    "argocd-server-url",
+    "argocd-admin-username",
+    "argocd-admin-password",
+  ]
+}
+#
+# 🟨 UAT - SECRETS
+#
+
+module "domain_uat_secrets" {
+  providers = {
+    azurerm = azurerm.uat
+  }
+
+  source = "./.terraform/modules/__v3__/key_vault_secrets_query"
+
+  key_vault_name = local.uat_kv_domain_name
+  resource_group = local.uat_kv_domain_resource_group
+
+  secrets = [
+    "argocd-server-url",
+    "argocd-admin-username",
+    "argocd-admin-password",
+  ]
+}
+
+#
+# 🛑 PROD - SECRETS
+#
+
+module "domain_prod_secrets" {
+  providers = {
+    azurerm = azurerm.prod
+  }
+
+  source = "./.terraform/modules/__v3__/key_vault_secrets_query"
+
+  key_vault_name = local.prod_kv_domain_name
+  resource_group = local.prod_kv_domain_resource_group
+
+  secrets = [
+    "argocd-server-url",
+    "argocd-admin-username",
+    "argocd-admin-password",
+  ]
+}
---
+++ azure-devops/shared-projects/99_locals.tf
@@ -0,0 +1,63 @@
+locals {
+  prefix            = "cstar"
+  domain            = "shared"
+  location_short    = "itn"
+  location          = "italynorth"
+  azdo_project_name = "cstar-platform-app-projects"
+  product           = "${local.prefix}-${local.location_short}"
+
+  devops_project_id = data.azuredevops_project.this.project_id
+
+  ### Agent Pool
+  azdo_agent_pool_dev  = "cstar-dev-linux"
+  azdo_agent_pool_uat  = "cstar-uat-linux"
+  azdo_agent_pool_prod = "cstar-prod-linux"
+
+  #
+  # Subscriptions
+  dev_subscription_name  = data.azurerm_subscriptions.dev.subscriptions[0].display_name
+  uat_subscription_name  = data.azurerm_subscriptions.uat.subscriptions[0].display_name
+  prod_subscription_name = data.azurerm_subscriptions.prod.subscriptions[0].display_name
+
+  dev_subscription_id  = data.azurerm_subscriptions.dev.subscriptions[0].subscription_id
+  uat_subscription_id  = data.azurerm_subscriptions.uat.subscriptions[0].subscription_id
+  prod_subscription_id = data.azurerm_subscriptions.prod.subscriptions[0].subscription_id
+
+
+  ### 🔑 Key Vault
+  dev_kv_domain_name            = "${local.prefix}-d-${local.location_short}-${local.domain}-kv"
+  dev_kv_domain_resource_group  = "${local.prefix}-d-${local.location_short}-${local.domain}-security-rg"
+  uat_kv_domain_name            = "${local.prefix}-u-${local.location_short}-${local.domain}-kv"
+  uat_kv_domain_resource_group  = "${local.prefix}-u-${local.location_short}-${local.domain}-security-rg"
+  prod_kv_domain_name           = "${local.prefix}-p-${local.location_short}-${local.domain}-kv"
+  prod_kv_domain_resource_group = "${local.prefix}-p-${local.location_short}-${local.domain}-security-rg"
+
+  ### 🌐 Network
+  rg_dev_dns_zone_name  = "cstar-d-vnet-rg"
+  rg_uat_dns_zone_name  = "cstar-u-vnet-rg"
+  rg_prod_dns_zone_name = "cstar-p-vnet-rg"
+
+  ### Idenity RG
+  dev_identity_rg_name  = "cstar-d-itn-srtp-cicd-rg"
+  uat_identity_rg_name  = "cstar-u-itn-srtp-cicd-rg"
+  prod_identity_rg_name = "cstar-p-itn-srtp-cicd-rg"
+
+  ### Service connections/endpoints Azure
+  dev_service_endpoint_azure_id    = data.azuredevops_serviceendpoint_azurerm.dev_azurerm_service_conn.service_endpoint_id
+  dev_service_endpoint_azure_name  = data.azuredevops_serviceendpoint_azurerm.dev_azurerm_service_conn.service_endpoint_name
+  uat_service_endpoint_azure_id    = data.azuredevops_serviceendpoint_azurerm.uat_azurerm_service_conn.service_endpoint_id
+  uat_service_endpoint_azure_name  = data.azuredevops_serviceendpoint_azurerm.uat_azurerm_service_conn.service_endpoint_name
+  prod_service_endpoint_azure_id   = data.azuredevops_serviceendpoint_azurerm.prod_azurerm_service_conn.service_endpoint_id
+  prod_service_endpoint_azure_name = data.azuredevops_serviceendpoint_azurerm.prod_azurerm_service_conn.service_endpoint_name
+
+}
+
+# LOCAL TLS CERT
+locals {
+  tlscert_repository = {
+    organization   = "pagopa"
+    name           = "le-azure-acme-tiny"
+    branch_name    = "refs/heads/master"
+    pipelines_path = "."
+  }
+}
