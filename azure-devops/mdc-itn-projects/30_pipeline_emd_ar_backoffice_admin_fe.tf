a6820ac4e54f1be35b5568aa439cb543c0fd38f5
Marco-Meloni-30 <marco.meloni@emeal.nttdata.com>
2026-08-07 10:17:45 +0200

    chore: MMC-963 Change baseUrl of backoffice on pipeline of admin-fe (#211)
    chore: change baseUrl of backoffice on pipeline of admin-fe

--- azure-devops/mdc-itn-projects/30_pipeline_emd_ar_backoffice_admin_fe.tf
+++ azure-devops/mdc-itn-projects/30_pipeline_emd_ar_backoffice_admin_fe.tf
@@ -17,24 +17,27 @@ locals {
   dev_keycloak_url                     = "https://api-mcshared.dev.cstar.pagopa.it/auth-itn"
   dev_keycloak_realm                   = "mdc"
   dev_keycloak_admin_client_id         = "ar-backoffice-portal-client"
+  dev_backoffice_internal_api_base_url = "https://api-emd.dev.cstar.pagopa.it/emd/backoffice-internal"
 
 
   uat_backoffice_admin_storage_account = replace("${local.prefix}-u-${local.location_short}-${local.domain}admin", "-", "")
   uat_keycloak_url                     = "https://api-mcshared.uat.cstar.pagopa.it/auth-itn"
   uat_keycloak_realm                   = "mdc"
   uat_keycloak_admin_client_id         = "ar-backoffice-portal-client"
+  uat_backoffice_internal_api_base_url = "https://api-emd.uat.cstar.pagopa.it/emd/backoffice-internal"
 
   prod_backoffice_admin_storage_account = replace("${local.prefix}-p-${local.location_short}-${local.domain}admin", "-", "")
   prod_keycloak_url                     = "https://api-mcshared.cstar.pagopa.it/auth-itn"
   prod_keycloak_realm                   = "mdc"
   prod_keycloak_admin_client_id         = "ar-backoffice-portal-client"
+  prod_backoffice_internal_api_base_url = "https://api-emd.cstar.pagopa.it/emd/backoffice-internal"
 
   emd_ar_backoffice_admin_fe_variables_deploy = {
     # AzureCLI@2 richiede il NOME della service connection (non l'id)
     DEV_AGENT_POOL         = local.azdo_agent_pool_dev
     DEV_AZURE_SUBSCRIPTION = local.dev_service_endpoint_azure_name
     DEV_STORAGE_ACCOUNT    = local.dev_backoffice_admin_storage_account
-    DEV_API_BASE_URL       = local.dev_backoffice_api_base_url
+    DEV_API_BASE_URL       = local.dev_backoffice_internal_api_base_url
     DEV_KEYCLOAK_URL       = local.dev_keycloak_url
     DEV_KEYCLOAK_REALM     = local.dev_keycloak_realm
     DEV_KEYCLOAK_CLIENT_ID = local.dev_keycloak_admin_client_id
@@ -42,7 +45,7 @@ locals {
     UAT_AGENT_POOL         = local.azdo_agent_pool_uat
     UAT_AZURE_SUBSCRIPTION = local.uat_service_endpoint_azure_name
     UAT_STORAGE_ACCOUNT    = local.uat_backoffice_admin_storage_account
-    UAT_API_BASE_URL       = local.uat_backoffice_api_base_url
+    UAT_API_BASE_URL       = local.uat_backoffice_internal_api_base_url
     UAT_KEYCLOAK_URL       = local.uat_keycloak_url
     UAT_KEYCLOAK_REALM     = local.uat_keycloak_realm
     UAT_KEYCLOAK_CLIENT_ID = local.uat_keycloak_admin_client_id
@@ -50,7 +53,7 @@ locals {
     PROD_AGENT_POOL         = local.azdo_agent_pool_prod
     PROD_AZURE_SUBSCRIPTION = local.prod_service_endpoint_azure_name
     PROD_STORAGE_ACCOUNT    = local.prod_backoffice_admin_storage_account
-    PROD_API_BASE_URL       = local.prod_backoffice_api_base_url
+    PROD_API_BASE_URL       = local.prod_backoffice_internal_api_base_url
     PROD_KEYCLOAK_URL       = local.prod_keycloak_url
     PROD_KEYCLOAK_REALM     = local.prod_keycloak_realm
     PROD_KEYCLOAK_CLIENT_ID = local.prod_keycloak_admin_client_id
